e18a4d83a34225f5cf6d19adca7e555b8d3096d9
Artsiom <artkostm@gmail.com>
2017-11-18 23:25:18 +0300

    add models

--- src/main/scala/com/artkostm/posters/model/schedule.scala
+++ src/main/scala/com/artkostm/posters/model/schedule.scala
@@ -1,5 +1,11 @@
 package com.artkostm.posters.model
 
-object schedule {
+import java.util.Date
 
-}
+case class Media(link: String, img: String)
+case class Description(desc: String, ticket: Option[String], isFree: Boolean)
+case class Event(media: Media, name: String, description: Description)
+case class Category(name: String, events: List[Event])
+
+sealed trait Schedule
+case class Day(events: List[Event], date: Date) extends Schedule
