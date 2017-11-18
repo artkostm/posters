c911189c0e940d75bfe671e90c33a663ee193433
Artsiom <artkostm@gmail.com>
2017-11-18 23:26:08 +0300

    add initial implementation of server layer

--- src/main/scala/com/artkostm/posters/PostersServer.scala
+++ src/main/scala/com/artkostm/posters/PostersServer.scala
@@ -1,5 +1,18 @@
 package com.artkostm.posters
 
-class PostersServer {
+import com.twitter.finagle.http.Request
+import com.twitter.finatra.http.{Controller, HttpServer}
+import com.twitter.finatra.http.routing.HttpRouter
 
+class PostersServer extends HttpServer {
+  override protected def configureHttp(router: HttpRouter): Unit =
+    router.add(new HelloController)
 }
+
+class HelloController extends Controller {
+  get("/hello") { request: Request =>
+    "Hello From Anmol. Let's Learn Finatra"
+  }
+}
+
+object Server extends PostersServer
