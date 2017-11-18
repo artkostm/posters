e2eea9092e4bb90dcff651e910b21f93bc38b443
Artsiom <artkostm@gmail.com>
2017-11-18 23:25:07 +0300

    basic setup

--- build.sbt
+++ build.sbt
@@ -3,4 +3,8 @@ name := "posters"
 version := "0.1"
 
 scalaVersion := "2.12.4"
-        
+
+resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
+resolvers += "Twitter Maven" at "https://maven.twttr.com"
+
+libraryDependencies ++= Dependencies.finatra
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,3 +1,16 @@
-object Dependencies {
+import sbt._
 
+object Dependencies {
+  lazy val versions = new {
+    val finatra = "17.11.0"
+    val logback = "1.2.3"
+    val scraper = "2.0.0"
+  }
+
+  val finatra_http = "com.twitter" %% "finatra-http" % versions.finatra
+  val logback = "ch.qos.logback" % "logback-classic" % versions.logback
+  val scraper = "net.ruippeixotog" %% "scala-scraper" % versions.scraper
+
+
+  lazy val finatra = Seq(finatra_http, logback, scraper)
 }
