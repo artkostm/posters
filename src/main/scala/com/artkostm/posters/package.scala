6453f2eef8e4aa219edfaf92ddc6b7d45ee1f88d
Artsiom <artkostm@gmail.com>
2017-11-19 16:36:59 +0300

    add configuration

--- src/main/scala/com/artkostm/posters/package.scala
+++ src/main/scala/com/artkostm/posters/package.scala
@@ -1,5 +1,42 @@
 package com.artkostm
 
-package object posters {
+import akka.actor.ActorSystem
+import com.typesafe.config.ConfigFactory
+import org.joda.time.format.{DateTimeFormat, DateTimeFormatter}
+import org.mapdb.DBMaker
 
+package object posters {
+  val DB_NAME = "postersMap"
+  lazy val actorSystem = ActorSystem("posters")
+  lazy val db = DBMaker.heapDB().make()
+  lazy val config = ConfigFactory.load()
+  lazy val scraperConfig = ScraperConfig(TutScraper(
+    config.getString("scraper.tut.url"),
+    DateTimeFormat.forPattern(config.getString("scraper.tut.format")),
+    config.getString("scraper.tut.blocksSelector"),
+    config.getString("scraper.tut.blockTitleSelector"),
+    config.getString("scraper.tut.eventsSelector"),
+    config.getString("scraper.tut.mediaSelector"),
+    config.getString("scraper.tut.eventNameSelector"),
+    config.getString("scraper.tut.descriptionSelector"),
+    config.getString("scraper.tut.descriptionTextSelector"),
+    config.getString("scraper.tut.ticketSelector"),
+    config.getString("scraper.tut.freeEventSelector"),
+    config.getString("scraper.tut.hrefAttrSelector"),
+    config.getString("scraper.tut.srcAttrSelector"),
+    config.getString("scraper.tut.imgSelector")
+  ))
+  lazy val httpConfig = HttpConfig(
+    s":${config.getString("http.port")}"
+  )
 }
+
+case class TutScraper(url: String, format: DateTimeFormatter, blocksSelector: String,
+                      blockTitleSelector: String, eventsSelector: String,
+                      mediaSelector: String, eventNameSelector: String,
+                      descriptionSelector: String, descriptionTextSelector: String,
+                      ticketSelector: String, freeEventSelector: String,
+                      hrefAttrSelector: String, srcAttrSelector: String,
+                      imgSelector: String)
+case class ScraperConfig(tut: TutScraper)
+case class HttpConfig(port: String)
