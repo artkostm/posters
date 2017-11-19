735879bf204c077dd1491e1b4ba5c22dd826f806
Artsiom <artkostm@gmail.com>
2017-11-19 16:36:46 +0300

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,12 +5,16 @@ object Dependencies {
     val finatra = "17.11.0"
     val logback = "1.2.3"
     val scraper = "2.0.0"
+    val akka = "2.5.6"
+    val mapdb = "3.0.5"
   }
 
   val finatra_http = "com.twitter" %% "finatra-http" % versions.finatra
   val logback = "ch.qos.logback" % "logback-classic" % versions.logback
   val scraper = "net.ruippeixotog" %% "scala-scraper" % versions.scraper
+  val akka_actor = "com.typesafe.akka" % "akka-actor_2.12" % versions.akka
+  val mapdb = "org.mapdb" % "mapdb" % versions.mapdb
+  val mapdbutils = "com.github.karasiq" %% "mapdbutils" % "1.1.1"
 
-
-  lazy val finatra = Seq(finatra_http, logback, scraper)
+  lazy val finatra = Seq(finatra_http, logback, scraper, akka_actor, mapdb, mapdbutils)
 }
