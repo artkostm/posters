8dc6a509b5b035a038cebdb123be217eed2a7051
Artsiom <artkostm@gmail.com>
2017-11-19 16:37:32 +0300

    server layer

--- src/main/scala/com/artkostm/posters/PostersServer.scala
+++ src/main/scala/com/artkostm/posters/PostersServer.scala
@@ -1,18 +1,45 @@
 package com.artkostm.posters
 
-import com.twitter.finagle.http.Request
+import com.artkostm.posters.scraper.Scraper
+import com.google.inject.Inject
+import com.google.inject.Singleton
+import com.twitter.finagle.http.{Request, Response}
+import com.twitter.finatra.http.exceptions.ExceptionMapper
+import com.twitter.finatra.http.response.ResponseBuilder
 import com.twitter.finatra.http.{Controller, HttpServer}
 import com.twitter.finatra.http.routing.HttpRouter
+import com.twitter.finatra.request.QueryParam
+import org.joda.time.DateTime
 
 class PostersServer extends HttpServer {
   override protected def configureHttp(router: HttpRouter): Unit =
-    router.add(new HelloController)
+    router
+      .exceptionMapper[IllegalArgumentExceptionHandler]
+      .add[ScheduleController]
 }
 
-class HelloController extends Controller {
-  get("/hello") { request: Request =>
-    "Hello From Anmol. Let's Learn Finatra"
+class ScheduleController extends Controller {
+  val scraper = new Scraper(scraperConfig)
+
+  get("/posters/categories/?") { request: CategoryRequest =>
+    request match {
+      case CategoryRequest(Some(date), Some(category), _) => scraper.scheduleFor(date).events.filter(_.name.equalsIgnoreCase(category))
+      case CategoryRequest(Some(date), _, Some(true)) => scraper.scheduleFor(date).events
+      case CategoryRequest(Some(date), None, None) => scraper.scheduleFor(date).events
+      case CategoryRequest(_, _, Some(false)) => scraper.scheduleFor(DateTime.now).events.map(_.name)
+    }
   }
 }
 
-object Server extends PostersServer
+@Singleton
+class IllegalArgumentExceptionHandler @Inject() (response: ResponseBuilder)
+  extends ExceptionMapper[Exception] {
+  override def toResponse(request: Request, throwable: Exception): Response = {
+    response.badRequest(ErrorResponse("1", throwable.getMessage))
+  }
+}
+
+case class CategoryRequest(@QueryParam date: Option[DateTime],
+                           @QueryParam category: Option[String],
+                           @QueryParam full: Option[Boolean])
+case class ErrorResponse(code: String, message: String)
