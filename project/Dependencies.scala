3186870c09e0a26b9a42d517eeee1a8bb7987b8a
Artsiom Chuiko <v-achuiko@expedia.com>
2017-11-23 15:10:37 +0300

    update dependencies & add slick 3.2.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,14 +6,16 @@ object Dependencies {
     val logback = "1.2.3"
     val scraper = "2.0.0"
     val akka = "2.5.6"
-    val mapdb = "3.0.5"
+	val slick = "3.2.1"
+	val slick_pg = "0.15.4"
   }
 
   val finatra_http = "com.twitter" %% "finatra-http" % versions.finatra
   val logback = "ch.qos.logback" % "logback-classic" % versions.logback
   val scraper = "net.ruippeixotog" %% "scala-scraper" % versions.scraper
   val akka_actor = "com.typesafe.akka" % "akka-actor_2.12" % versions.akka
-  val mapdb = "org.mapdb" % "mapdb" % versions.mapdb
+  val slick = "com.typesafe.slick" %% "slick" % versions.slick
+  val slick_pg = "com.github.tminglei" %% "slick-pg" % versions.slick_pg
 
-  lazy val finatra = Seq(finatra_http, logback, scraper, akka_actor, mapdb)
+  lazy val finatra = Seq(finatra_http, logback, scraper, akka_actor, slick, slick_pg)
 }
