63a7e0bfbc5f5cdb0d2a9b03a56eebf80a96ec8d
Artsiom <artkostm@gmail.com>
2017-11-24 00:49:06 +0300

    add table model

--- src/main/scala/com/artkostm/posters/model/db.scala
+++ src/main/scala/com/artkostm/posters/model/db.scala
@@ -1,5 +1,23 @@
 package com.artkostm.posters.model
 
-object db {
+import java.util.Date
 
+import slick.jdbc.H2Profile.api._
+import slick.lifted.ProvenShape
+
+class Events(tag: Tag) extends Table[(String, Date, String, String)](tag, "events") {
+  implicit val d2t = DateMapper.DateTime2SqlTimestampMapper
+  def category: Rep[String] = column[String]("category")
+  def date: Rep[Date] = column[Date]("date")
+  def eventName: Rep[String] = column[String]("event_date")
+  def ids: Rep[String] = column[String]("ids")
+
+  def * : ProvenShape[(String, Date, String, String)] = (category, date, eventName, ids)
+  def pk = primaryKey("pk_events", (category, date, eventName))
+}
+
+object DateMapper {
+  val DateTime2SqlTimestampMapper = MappedColumnType.base[Date, java.sql.Timestamp](
+    { utilDate => new java.sql.Timestamp(utilDate.getTime) },
+    { sqlTimestamp => new Date(sqlTimestamp.getTime()) })
 }
