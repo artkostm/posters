ea884a33a69f879103731c536c794f17762707f5
Artsiom <artkostm@gmail.com>
2017-11-24 21:44:41 +0300

    add db components

--- src/main/scala/com/artkostm/posters/repository/components.scala
+++ src/main/scala/com/artkostm/posters/repository/components.scala
@@ -1,5 +1,17 @@
 package com.artkostm.posters.repository
 
-object components {
+trait H2DbComponent extends DbComponent {
+  override val driver = slick.jdbc.H2Profile
 
+  import driver.api._
+
+  override val db: driver.api.Database = Database.forConfig("h2mem1")
+}
+
+trait PostgresDbComponent extends DbComponent {
+  override val driver = slick.jdbc.PostgresProfile
+
+  import driver.api._
+
+  override val db: driver.api.Database = _
 }
