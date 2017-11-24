5ca245648537bfa6860ca0ede24c595dbde45486
Artsiom <artkostm@gmail.com>
2017-11-25 00:23:24 +0300

    update example

--- src/main/scala/com/artkostm/posters/repository/slick.scala
+++ src/main/scala/com/artkostm/posters/repository/slick.scala
@@ -1,38 +1,26 @@
-package com.artkostm.posters.postgres
+package com.artkostm.posters.repository
 
-import java.util.Date
-
-import com.artkostm.posters._
-import com.artkostm.posters.model.Events
-import slick.dbio.DBIOAction
-import slick.jdbc.meta.MTable
-import slick.lifted.TableQuery
-import slick.jdbc.H2Profile.api._
+import com.artkostm.posters.model.Assign
+import org.joda.time.DateTime
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.{Await, Future}
+import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 
 object slicK extends App {
 
-  val db = Database.forConfig("h2mem1")
+  H2EventsRepository.setUp
   try {
-    val date = new Date()
-    val f = db.run(DBIO.seq(
-      EventPersister.setupAction,
-      EventPersister.get.result.map(r => println(s"After SetUp: $r")),
-      EventPersister.saveAction("Кино", date, "Stranger things", "3,5"),
-      EventPersister.get.result.map(r => println(s"After Create: $r")),
-      EventPersister.saveAction("Кино", date, "Stranger things", "3,5,6"),
-      EventPersister.get.result.map(r => println(s"After Update: $r")),
-      EventPersister.saveAction("Цирк", new Date, "Медведи", "1"),
-      EventPersister.get.result.map(r => println(s"After Create: $r")),
-      EventPersister.events.delete,
-      EventPersister.get.result.map(r => println(s"After Delete: $r")),
-      EventPersister.events.schema.drop
-    ))
+    val date = DateTime.now
+    val f = for {
+      _ <- H2EventsRepository.all.map(list => println(s"Before saving: $list"))
+      saved <- H2EventsRepository.save(Assign("Кино", date, "мстители", "6"))
+      _ <- H2EventsRepository.all.map(list => println(s"After saving: $list ===> saved: $saved"))
+      updated <- H2EventsRepository.save(Assign("Кино", date, "мстители", "6,7"))
+      _ <- H2EventsRepository.all.map(list => println(s"After updating: $list ===> saved: $saved"))
+    } yield ()
 
     Await.result(f, Duration.Inf)
 
-  } finally db.close()
+  } finally H2EventsRepository.db.close()
 }
