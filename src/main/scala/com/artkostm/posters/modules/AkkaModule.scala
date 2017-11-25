5fa6aba7347cad4c61fdf71041288cc9a6d397e7
Artsiom <artkostm@gmail.com>
2017-11-25 13:38:56 +0300

    add akka module

--- src/main/scala/com/artkostm/posters/modules/AkkaModule.scala
+++ src/main/scala/com/artkostm/posters/modules/AkkaModule.scala
@@ -1,5 +1,30 @@
 package com.artkostm.posters.modules
 
-object AkkaModule {
+import akka.actor.SupervisorStrategy.Stop
+import akka.actor.{Actor, ActorRef, Props, Terminated}
+import com.artkostm.posters.actorSystem
+import com.twitter.inject.{Injector, TwitterModule}
 
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext, Future}
+
+object AkkaModule extends TwitterModule {
+  private val actor: ActorRef = actorSystem.actorOf(Props[ShutdownActor])
+
+  override def singletonStartup(injector: Injector): Unit = Runtime.getRuntime.addShutdownHook(new Thread(() => {
+      val terminate: Future[Terminated] = actorSystem.terminate()
+      Await.result(terminate, Duration("10 seconds"))
+    }
+  ))
+
+  override def singletonShutdown(injector: Injector): Unit = actor ! Stop
+}
+
+private[modules] class ShutdownActor extends Actor {
+  override def receive: Receive = {
+    case Stop =>
+      println("Stopping application")
+      implicit val executionContext: ExecutionContext = context.system.dispatcher
+      context.system.scheduler.scheduleOnce(Duration.Zero)(System.exit(1))
+  }
 }
--- src/main/scala/com/artkostm/posters/modules/DbModule.scala
+++ src/main/scala/com/artkostm/posters/modules/DbModule.scala
@@ -1,36 +1,17 @@
-package com.artkostm.posters.repository
+package com.artkostm.posters.modules
 
-import akka.actor.{Actor, ActorRef, Props, Terminated}
-import akka.actor.SupervisorStrategy.Stop
-import com.twitter.inject.{Injector, TwitterModule}
 import com.artkostm.posters._
-
-import scala.concurrent.{Await, ExecutionContext, Future}
-import scala.concurrent.duration.Duration
+import com.artkostm.posters.repository.H2EventsRepository
+import com.twitter.inject.{Injector, TwitterModule}
 
 object DbModule extends TwitterModule {
   implicit val ec = actorSystem.dispatcher
-  private val actor: ActorRef = actorSystem.actorOf(Props[ShutdownActor])
 
   override def singletonStartup(injector: Injector): Unit = {
-    PostgresEventsRepository.setUp
-    Runtime.getRuntime.addShutdownHook(new Thread(() => {
-      val terminate: Future[Terminated] = actorSystem.terminate()
-      Await.result(terminate, Duration("10 seconds"))
-    }))
+    H2EventsRepository.setUp
   }
 
   override def singletonShutdown(injector: Injector): Unit = {
-    PostgresEventsRepository.db.close()
-    actor ! Stop
-  }
-}
-
-class ShutdownActor extends Actor {
-  override def receive: Receive = {
-    case Stop =>
-      println("Stopping application")
-      implicit val executionContext: ExecutionContext = context.system.dispatcher
-      context.system.scheduler.scheduleOnce(Duration.Zero)(System.exit(1))
+    H2EventsRepository.db.close
   }
 }
