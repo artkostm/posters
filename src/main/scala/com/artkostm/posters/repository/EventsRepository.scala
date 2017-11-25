c5ebedd32463c07c53544fdd0ef1a7e2cff9be47
Artsiom <artkostm@gmail.com>
2017-11-25 13:39:31 +0300

    fix repository

--- src/main/scala/com/artkostm/posters/repository/EventsRepository.scala
+++ src/main/scala/com/artkostm/posters/repository/EventsRepository.scala
@@ -5,17 +5,18 @@ import org.joda.time.DateTime
 import slick.dbio.DBIOAction
 import slick.jdbc.meta.MTable
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 trait EventsRepository extends EventsTable { this: DbComponent =>
   import driver.api._
 
-  def setUp: Future[Unit] = db.run {
-    DBIO.seq(MTable.getTables.flatMap(v => {
+  def setUp()(implicit ctx: ExecutionContext): Future[Unit] = db.run {
+    val createIfNotExist = MTable.getTables.flatMap(v => {
       val names = v.map(_.name.name)
       if (!names.contains(eventsTableQuery.baseTableRow.tableName)) eventsTableQuery.schema.create
-      else DBIOAction.from(Future.successful())
-    }), eventsTableQuery.filter(event => event.date < DateTime.now).delete)
+      else DBIOAction.successful()
+    })
+    DBIO.seq(createIfNotExist, eventsTableQuery.filter(event => event.date < DateTime.now).delete)
   }
 
   def save(assign: Assign): Future[Int] = db.run(eventsTableQuery.insertOrUpdate(assign))
@@ -23,6 +24,6 @@ trait EventsRepository extends EventsTable { this: DbComponent =>
   def all: Future[List[Assign]] = db.run(eventsTableQuery.to[List].result)
 
   def find(category: String, date: DateTime, eventName: String): Future[Option[Assign]] = db.run {
-    eventsTableQuery.filter(a => a.category == category && a.eventName == eventName && a.date == date).result.headOption
+    eventsTableQuery.filter(a => a.category === category && a.eventName === eventName && a.date === date).result.headOption
   }
 }
--- src/main/scala/com/artkostm/posters/repository/slick.scala
+++ src/main/scala/com/artkostm/posters/repository/slick.scala
@@ -1,23 +1,24 @@
 package com.artkostm.posters.repository
 
+import com.artkostm.posters._
 import com.artkostm.posters.model.Assign
 import org.joda.time.DateTime
 
-import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 
-object slicK extends App {
+object slicK {//extends App {
 
-  H2EventsRepository.setUp
+  implicit val ec = actorSystem.dispatcher
   try {
     val date = DateTime.now
     val f = for {
+      _ <- H2EventsRepository.setUp
       _ <- H2EventsRepository.all.map(list => println(s"Before saving: $list"))
       saved <- H2EventsRepository.save(Assign("Кино", date, "мстители", "6"))
       _ <- H2EventsRepository.all.map(list => println(s"After saving: $list ===> saved: $saved"))
       updated <- H2EventsRepository.save(Assign("Кино", date, "мстители", "6,7"))
-      _ <- H2EventsRepository.all.map(list => println(s"After updating: $list ===> saved: $saved"))
+      _ <- H2EventsRepository.all.map(list => println(s"After updating: $list ===> updated: $updated"))
     } yield ()
 
     Await.result(f, Duration.Inf)
