3d082b590866a967178b1dc3603af882f8a26c17
Artsiom <artkostm@gmail.com>
2017-12-04 00:09:16 +0300

    add table models

--- src/main/scala/com/artkostm/posters/model/db.scala
+++ src/main/scala/com/artkostm/posters/model/db.scala
@@ -1,11 +1,11 @@
 package com.artkostm.posters.model
 
 
-import com.artkostm.posters.repository.DbComponent
+import com.artkostm.posters.repository.{JsonSupportDbComponent, DbComponent}
 import org.joda.time.DateTime
 import slick.lifted.ProvenShape
 
-trait EventsTable { this: DbComponent =>
+trait AssignTable { this: DbComponent =>
   import driver.api._
   protected implicit val DateTime2SqlTimestampMapper = MappedColumnType.base[DateTime, java.sql.Timestamp]({
     date => new java.sql.Timestamp(date.toDate.getTime)
@@ -13,7 +13,7 @@ trait EventsTable { this: DbComponent =>
     sqlTimestamp => new DateTime(sqlTimestamp.getTime())
   })
 
-  private[EventsTable] class Events(tag: Tag) extends Table[Assign](tag, "events") {
+  private[AssignTable] class Assignees(tag: Tag) extends Table[Assign](tag, "assignees") {
     def category: Rep[String] = column[String]("category")
     def date: Rep[DateTime] = column[DateTime]("date")
     def eventName: Rep[String] = column[String]("event_name")
@@ -23,7 +23,37 @@ trait EventsTable { this: DbComponent =>
     def pk = primaryKey("pk_events", (category, date, eventName))
   }
 
-  protected val eventsTableQuery = TableQuery[Events]
+  protected val assigneesTableQuery = TableQuery[Assignees]
+}
+
+trait EventsDayTable { this: JsonSupportDbComponent =>
+  import driver.plainAPI._
+
+  private[EventsDayTable] class Days(tag: Tag) extends Table[EventsDay](tag, "days") {
+    def date: Rep[DateTime] = column[DateTime]("date")
+    def categories: Rep[List[Category]] = column[List[Category]]("categories")
+
+    def * : ProvenShape[EventsDay] = (date, categories) <> (EventsDay.tupled, EventsDay.unapply)
+    def pk = primaryKey("pk_days", date)
+  }
+
+  protected val daysTableQuery = TableQuery[Days]
+}
+
+trait InfoTable { this: JsonSupportDbComponent =>
+  import driver.plainAPI._
+
+  private[InfoTable] class Information(tag: Tag) extends Table[Info](tag, "info") {
+    def link: Rep[String] = column[String]("link")
+    def eventsInfo: Rep[EventInfo] = column[EventInfo]("eventsInfo")
+
+    def * : ProvenShape[Info] = (link, eventsInfo) <> (Info.tupled, Info.unapply)
+    def pk = primaryKey("pk_info", link)
+  }
+
+  protected val infoTableQuery = TableQuery[Information]
 }
 
 case class Assign(category: String, date: DateTime, eventName: String, ids: String)
+case class EventsDay(date: DateTime, categories: List[Category])
+case class Info(link: String, eventInfo: EventInfo)
