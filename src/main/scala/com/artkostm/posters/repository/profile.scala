2156544e1ad9055fe33490d99ad488278e0b80e7
Artsiom <artkostm@gmail.com>
2017-12-03 19:44:40 +0300

    update db profile

--- src/main/scala/com/artkostm/posters/repository/profile.scala
+++ src/main/scala/com/artkostm/posters/repository/profile.scala
@@ -1,6 +1,6 @@
 package com.artkostm.posters.repository
 
-import com.artkostm.posters.model.Category
+import com.artkostm.posters.model.{Category, EventInfo}
 import com.github.tminglei.slickpg.PgPlayJsonSupport
 import com.github.tminglei.slickpg.array.PgArrayJdbcTypes
 import com.github.tminglei.slickpg.utils.SimpleArrayUtils
@@ -10,6 +10,7 @@ import slick.jdbc.PostgresProfile
 trait HerokuPostgresProfile extends PostgresProfile
                                with PgPlayJsonSupport
                                with PgArrayJdbcTypes {
+
   override val pgjson = "jsonb"
 
   override val api: API = new PostersDBAPI {}
@@ -18,10 +19,15 @@ trait HerokuPostgresProfile extends PostgresProfile
 
   trait PostersDBAPI extends super.API with JsonImplicits {
     import Category._
+    import EventInfo._
     implicit val categoryJsonTypeMapper = MappedJdbcType.base[Category, JsValue](Json.toJson(_), _.as[Category])
+    implicit val eventInfoJsonTypeMapper = MappedJdbcType.base[EventInfo, JsValue](Json.toJson(_), _.as[EventInfo])
     implicit val categoryArrayTypeMapper = new AdvancedArrayJdbcType[Category](pgjson,
       (s) => SimpleArrayUtils.fromString[Category](Json.parse(_).as[Category])(s).orNull,
       (v) => SimpleArrayUtils.mkString[Category](c => Json.stringify(Json.toJson(c)))(v)).to(_.toList)
+    implicit val eventInfoArrayTypeMapper = new AdvancedArrayJdbcType[EventInfo](pgjson,
+      (s) => SimpleArrayUtils.fromString[EventInfo](Json.parse(_).as[EventInfo])(s).orNull,
+      (v) => SimpleArrayUtils.mkString[EventInfo](c => Json.stringify(Json.toJson(c)))(v)).to(_.toList)
   }
 }
 
