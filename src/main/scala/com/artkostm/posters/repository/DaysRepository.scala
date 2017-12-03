4cc48c90064818907e923c721656250f85a36522
Artsiom <artkostm@gmail.com>
2017-12-04 00:10:02 +0300

    add day repo

--- src/main/scala/com/artkostm/posters/repository/DaysRepository.scala
+++ src/main/scala/com/artkostm/posters/repository/DaysRepository.scala
@@ -1,5 +1,27 @@
 package com.artkostm.posters.repository
 
-trait DaysRepository {
+import com.artkostm.posters.model.{EventsDay, EventsDayTable}
+import org.joda.time.DateTime
+import slick.dbio.DBIOAction
+import slick.jdbc.meta.MTable
 
+import scala.concurrent.{ExecutionContext, Future}
+
+trait DaysRepository extends EventsDayTable { this: JsonSupportDbComponent =>
+  import driver.plainAPI._
+
+  def setUpDays()(implicit ctx: ExecutionContext): Future[Unit] = db.run {
+    val createIfNotExist = MTable.getTables.flatMap(v => {
+      val names = v.map(_.name.name)
+      if (!names.contains(daysTableQuery.baseTableRow.tableName)) daysTableQuery.schema.create
+      else DBIOAction.successful()
+    })
+    DBIO.seq(createIfNotExist, daysTableQuery.filter(day => day.date < DateTime.now.minusDays(1)).delete)
+  }
+
+  def save(day: EventsDay): Future[Int] = db.run(daysTableQuery.insertOrUpdate(day.copy(date = day.date.withTimeAtStartOfDay())))
+
+  def find(date: DateTime): Future[Option[EventsDay]] = db.run {
+    daysTableQuery.filter(d => d.date === date.withTimeAtStartOfDay()).result.headOption
+  }
 }
