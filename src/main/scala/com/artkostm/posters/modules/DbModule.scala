77b73a66e4e6a60e23bd6c9202c7f4a5360f8efa
Artsiom <artkostm@gmail.com>
2017-12-04 00:09:32 +0300

    small update

--- src/main/scala/com/artkostm/posters/modules/DbModule.scala
+++ src/main/scala/com/artkostm/posters/modules/DbModule.scala
@@ -1,17 +1,17 @@
 package com.artkostm.posters.modules
 
 import com.artkostm.posters._
-import com.artkostm.posters.repository.H2EventsRepository
+import com.artkostm.posters.repository.H2AssignRepository
 import com.twitter.inject.{Injector, TwitterModule}
 
 object DbModule extends TwitterModule {
   implicit val ec = actorSystem.dispatcher
 
   override def singletonStartup(injector: Injector): Unit = {
-    H2EventsRepository.setUp
+    H2AssignRepository.setUpAssign
   }
 
   override def singletonShutdown(injector: Injector): Unit = {
-    H2EventsRepository.db.close
+    H2AssignRepository.db.close
   }
 }
