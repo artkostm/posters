242df50f4f8b9c01065a257f42240b1036ef9404
Artsiom <artkostm@gmail.com>
2017-12-03 17:10:50 +0300

    formatting

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,26 +2,26 @@ import sbt._
 
 object Dependencies {
   lazy val versions = new {
-    val finatra = "17.11.0"
-    val logback = "1.2.3"
-    val scraper = "2.0.0"
-    val akka = "2.5.6"
-    val slick = "3.2.1"
+    val finatra  = "17.11.0"
+    val logback  = "1.2.3"
+    val scraper  = "2.0.0"
+    val akka     = "2.5.6"
+    val slick    = "3.2.1"
     val slick_pg = "0.15.4"
     val hikaricp = "3.2.1"
     val postgres = "9.4.1208"
   }
 
-  val finatra_http = "com.twitter" %% "finatra-http" % versions.finatra
-  val logback = "ch.qos.logback" % "logback-classic" % versions.logback
-  val scraper = "net.ruippeixotog" %% "scala-scraper" % versions.scraper
-  val akka_actor = "com.typesafe.akka" % "akka-actor_2.12" % versions.akka
-  val slick = "com.typesafe.slick" %% "slick" % versions.slick
-  val slick_pg = "com.github.tminglei" %% "slick-pg" % versions.slick_pg
-  val slick_pg_json4s = "com.github.tminglei" %% "slick-pg_json4s" % versions.slick_pg
-  val hikaricp = "com.typesafe.slick" %% "slick-hikaricp" % versions.hikaricp
-  val postgres = "org.postgresql" % "postgresql" % versions.postgres
-  val akka_streams = "com.typesafe.akka" %% "akka-stream" % versions.akka
+  val finatra_http    = "com.twitter"         %%  "finatra-http"    % versions.finatra
+  val logback         = "ch.qos.logback"      %   "logback-classic" % versions.logback
+  val scraper         = "net.ruippeixotog"    %%  "scala-scraper"   % versions.scraper
+  val akka_actor      = "com.typesafe.akka"   %   "akka-actor_2.12" % versions.akka
+  val slick           = "com.typesafe.slick"  %%  "slick"           % versions.slick
+  val slick_pg        = "com.github.tminglei" %%  "slick-pg"        % versions.slick_pg
+  val slick_pg_json4s = "com.github.tminglei" %%  "slick-pg_json4s" % versions.slick_pg
+  val hikaricp        = "com.typesafe.slick"  %%  "slick-hikaricp"  % versions.hikaricp
+  val postgres        = "org.postgresql"      %   "postgresql"      % versions.postgres
+  val akka_streams    = "com.typesafe.akka"   %%  "akka-stream"     % versions.akka
 
 
   lazy val all = Seq(finatra_http, logback, scraper, akka_actor, slick,
