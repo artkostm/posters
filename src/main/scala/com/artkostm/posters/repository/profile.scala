b89244abce8daadd7478c208066f06e865831378
Artsiom <artkostm@gmail.com>
2017-12-03 19:42:20 +0300

    add custom postgres profile

--- src/main/scala/com/artkostm/posters/repository/profile.scala
+++ src/main/scala/com/artkostm/posters/repository/profile.scala
@@ -1,5 +1,28 @@
 package com.artkostm.posters.repository
 
-object profile {
+import com.artkostm.posters.model.Category
+import com.github.tminglei.slickpg.PgPlayJsonSupport
+import com.github.tminglei.slickpg.array.PgArrayJdbcTypes
+import com.github.tminglei.slickpg.utils.SimpleArrayUtils
+import play.api.libs.json.{JsValue, Json}
+import slick.jdbc.PostgresProfile
 
+trait HerokuPostgresProfile extends PostgresProfile
+                               with PgPlayJsonSupport
+                               with PgArrayJdbcTypes {
+  override val pgjson = "jsonb"
+
+  override val api: API = new PostersDBAPI {}
+
+  val plainAPI = new PostersDBAPI with PlayJsonPlainImplicits
+
+  trait PostersDBAPI extends super.API with JsonImplicits {
+    import Category._
+    implicit val categoryJsonTypeMapper = MappedJdbcType.base[Category, JsValue](Json.toJson(_), _.as[Category])
+    implicit val categoryArrayTypeMapper = new AdvancedArrayJdbcType[Category](pgjson,
+      (s) => SimpleArrayUtils.fromString[Category](Json.parse(_).as[Category])(s).orNull,
+      (v) => SimpleArrayUtils.mkString[Category](c => Json.stringify(Json.toJson(c)))(v)).to(_.toList)
+  }
 }
+
+object HerokuPostgresProfile extends HerokuPostgresProfile
