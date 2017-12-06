6c2fbf9e7c24b4b06f713a45dee771267ef8ba23
Artsiom <artkostm@gmail.com>
2017-12-06 19:56:01 +0300

    collector implementation

--- src/main/scala/com/artkostm/posters/collector/collector.scala
+++ src/main/scala/com/artkostm/posters/collector/collector.scala
@@ -1,87 +1,59 @@
 package com.artkostm.posters.collector
 
-import java.util.Date
-
-import akka.stream.{ActorMaterializer, FlowShape, ThrottleMode}
-import akka.stream.scaladsl.{Balance, BidiFlow, Broadcast, Flow, GraphDSL, Keep, Merge, Sink, Source}
+import akka.NotUsed
+import akka.stream.{ActorMaterializer, ClosedShape}
+import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, RunnableGraph, Sink, Source}
 import com.artkostm.posters.model._
-import org.joda.time.{DateTime, DateTimeZone}
+import org.joda.time.DateTime
 import com.artkostm.posters._
+import com.artkostm.posters.repository.{DaysRepository, InfoRepository, JsonSupportDbComponent}
 import com.artkostm.posters.scraper.EventsScraper
-import play.api.libs.json.Json
 
 import scala.concurrent.Future
-import scala.concurrent.duration._
 
-object C1 extends App {
-  val storage = scala.collection.mutable.Map.empty[String, List[Category]]
-  val month = (0 to 30).map(DateTime.now().plusDays)
+class EventsCollector(scraper: EventsScraper, days: Seq[DateTime],
+                      repository: DaysRepository with InfoRepository) {
+  implicit val system = actorSystem
+  implicit val materializer = ActorMaterializer()
+  implicit val ec = system.dispatcher
 
-//  implicit val system = actorSystem
-//  implicit val materializer = ActorMaterializer()
-//  implicit val ec = system.dispatcher
+  private val g = RunnableGraph.fromGraph(GraphDSL.create() { implicit builder: GraphDSL.Builder[NotUsed] =>
+    import GraphDSL.Implicits._
+    val in = Source.fromIterator[DateTime](() => days.toIterator)
+      //.throttle(5, 1 second, 1, ThrottleMode.shaping)
+      .mapAsyncUnordered(5)(date => Future(scraper.scheduleFor(date)))
 
-  object Cat {
-    import play.api.libs.json._
-    implicit val desFmt = Json.format[Description]
-    implicit val desWrt = Json.writes[Description]
+    val eventsInfoFlow = Flow[Day]
+      .flatMapConcat(d => Source.fromIterator(() => d.events.flatMap(_.events).toIterator))
+      //.throttle(5, 1 second, 1, ThrottleMode.shaping)
+      .mapAsyncUnordered(5)(event => Future(scraper.eventInfo(event.media.link)))
 
-    implicit val medFmt = Json.format[Media]
-    implicit val medWrt = Json.writes[Media]
+    val out = Sink.ignore
+    val bcast = builder.add(Broadcast[Day](2))
 
-    implicit val evnFmt = Json.format[Event]
-    implicit val evnWrt = Json.writes[Event]
+    val saveDayFlow = Flow[Day].map(day => EventsDay(new DateTime(day.date), day.events)).mapAsyncUnordered(5)(repository.save)
 
-    implicit val catFmt = Json.format[Category]
-    implicit val catWrt = Json.writes[Category]
+    val saveInfoFlow = Flow[Option[Info]].mapAsyncUnordered(5) {
+      case Some(info) => repository.save(info)
+      case None => Future.failed(new RuntimeException)
+    }
 
-    implicit val dayFmt = Json.format[Day]
-    implicit val dayWrt = Json.writes[Day]
-  }
+    in ~> bcast ~> saveDayFlow                    ~> out
+          bcast ~> eventsInfoFlow ~> saveInfoFlow ~> out
+    ClosedShape
+  })
 
-  import Cat._
-  //println(Json.toJson(Day(List(Category("movie", List(Event(Media("", ""), "", Description("description here", Some("50$"), false))))), new Date)).toString())
-
-  println(Json.parse(
-    """
-      |{"events":[{"name":"movie","events":[{"media":{"link":"http;//lisiy-korch","img":"KARTINKA"},"name":"","description":{"desc":"description here","ticket":"50$","isFree":false}}]}],"date":1512316681373}
-    """.stripMargin).as[Day])
-
-//  val scraper = new EventsScraper(scraperConfig)
-//
-//  val done = Source.fromIterator[DateTime](() => month.toIterator)
-//    .throttle(3, 1 second, 1, ThrottleMode.shaping)
-//    .mapAsyncUnordered(3)(date => Future(scraper.scheduleFor(date)))
-//    .toMat(Sink.foreach(println))(Keep.right).run()
-//
-//  done.onComplete(_ => actorSystem.terminate())
-
-//  import GraphDSL.Implicits._
-//  val partial = GraphDSL.create() { implicit builder =>
-//    val B = builder.add(Broadcast[Int](2))
-//    val C = builder.add(Merge[Int](2).async)
-//    val E = builder.add(Balance[Int](2))
-//    val F = builder.add(Merge[Int](2))
-//
-//                C <~ F
-//    B  ~>    Flow[Int].map(id => {println(s"B -> C: $id"); id})  ~>   C ~> F
-//    B  ~>  Flow[Int].map(id => {println(s"B -> E: $id"); id})  ~>  E  ~> Flow[Int].map(id => {println(s"E -> F: $id"); id}) ~>  F
-//    FlowShape(B.in, E.out(1))
-//  }.named("partial")
-//
-//
-//  val (_, done) = Source.fromIterator[Int](() => (1 to 6).toIterator)
-//    .throttle(1, 1 second, 1, ThrottleMode.shaping)
-//    .via(partial)
-//    .toMat(Sink.foreach(println))(Keep.both).run()
-//
-//  done.onComplete(_ => actorSystem.terminate())
-//
-//  case class Message(content: String)
-//
-//  def toStr(message: Message): String = message.content
-//  def fromStr(str: String): Message = Message(str)
-//
-//
-//  val codec = BidiFlow.fromFunctions(toStr, fromStr)
+  def run() = g.run()
 }
+
+
+//object lol extends App {
+//  val month = (0 to 30).map(DateTime.now().plusDays).map(_.withTimeAtStartOfDay())
+//  val collector = new EventsCollector(new EventsScraper(scraperConfig), month, new JsonSupportDbComponent with DaysRepository with InfoRepository {
+//    override def save(day: EventsDay): Future[Int] = { println(day); Future.successful(0) }
+//
+//    override def save(info: Info): Future[Int] = { println(info); Future.successful(0) }
+//  })
+//
+//  collector.run()
+//}
