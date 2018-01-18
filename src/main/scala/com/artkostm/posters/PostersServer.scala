e95ebb1e24d7d97ce58b1f3a209c63ae6b1bde1c
Artsiom <artkostm@gmail.com>
2018-01-18 23:15:58 +0300

    add webhook endpiont

--- src/main/scala/com/artkostm/posters/PostersServer.scala
+++ src/main/scala/com/artkostm/posters/PostersServer.scala
@@ -1,25 +1,38 @@
 package com.artkostm.posters
 
+import com.artkostm.posters.dialog.DialogflowRequest
 import com.artkostm.posters.model.Assign
 import com.artkostm.posters.modules.{AkkaModule, DbModule}
 import com.artkostm.posters.repository.PostgresPostersRepository
+import com.fasterxml.jackson.databind.PropertyNamingStrategy
+import com.fasterxml.jackson.databind.module.SimpleModule
 import com.google.inject.{Inject, Module, Singleton}
 import com.twitter.finagle.http.{Request, Response}
 import com.twitter.finatra.http.exceptions.ExceptionMapper
+import com.twitter.finatra.http.filters.{CommonFilters, LoggingMDCFilter}
 import com.twitter.finatra.http.response.ResponseBuilder
 import com.twitter.finatra.http.{Controller, HttpServer}
 import com.twitter.finatra.http.routing.HttpRouter
+import com.twitter.finatra.json.modules.FinatraJacksonModule
 import com.twitter.finatra.request.QueryParam
 import org.joda.time.DateTime
 
 import scala.concurrent.Future
 
+object PostersJacksonModule extends FinatraJacksonModule {
+  override protected val propertyNamingStrategy = PropertyNamingStrategy.LOWER_CAMEL_CASE
+}
+
 class PostersServer extends HttpServer {
   override val defaultFinatraHttpPort: String = httpConfig.port
-  override protected def modules: Seq[Module] = Seq(DbModule, AkkaModule)
+  override protected def disableAdminHttpServer = true
+  override protected def jacksonModule = PostersJacksonModule
+  //override protected def modules: Seq[Module] = Seq(DbModule, AkkaModule)
   override protected def configureHttp(router: HttpRouter): Unit =
     router
       .exceptionMapper[IllegalArgumentExceptionHandler]
+      .filter[LoggingMDCFilter[Request, Response]]
+      .filter[CommonFilters]
       .add[ScheduleController]
 }
 
@@ -58,6 +71,11 @@ class ScheduleController extends Controller {
   
   get("/posters/eventinfo/?") { request: EventInfoRequest =>
     eventsScraper.eventInfo(request.link)
+  }
+
+  post("/posters/webhook/?") { request: DialogflowRequest =>
+    println(request)
+    //request
   }
 }
 
