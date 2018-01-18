ecbe83d6c1d933d480c1777803bf08e91bbea52d
Artsiom <artkostm@gmail.com>
2018-01-18 23:35:32 +0300

    add key data extractor

--- src/main/scala/com/artkostm/posters/dialog/FlowKeyDataExtractor.scala
+++ src/main/scala/com/artkostm/posters/dialog/FlowKeyDataExtractor.scala
@@ -1,5 +1,25 @@
 package com.artkostm.posters.dialog
 
-class FlowKeyDataExtractor {
+import org.joda.time.DateTime
 
+case class FlowKeyData(category: List[String], date: Option[DateTime], period: Option[String])
+
+trait FlowKeyDataExtractor {
+  def actionIncomplete(req: DialogflowRequest): Boolean = req.result.actionIncomplete
+
+  def extract(request: DialogflowRequest): FlowKeyData = {
+    val category = request.result.parameters.category
+    val date = request.result.parameters.datetime.date
+    val period = request.result.parameters.datetime.period
+    FlowKeyData(category, date, period)
+  }
+}
+
+object FlowKeyDataExtractor extends FlowKeyDataExtractor {
+  private val allCategories = "Все"
+
+  def shouldShowAll(request: DialogflowRequest): Boolean = {
+    val category = request.result.parameters.category
+    category.size == 1 && category.contains(allCategories)
+  }
 }
