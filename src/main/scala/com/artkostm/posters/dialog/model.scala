723ce983dfc55a190f73251462fc33a0c21b05c8
Artsiom <artkostm@gmail.com>
2018-01-18 23:16:17 +0300

    add models

--- src/main/scala/com/artkostm/posters/dialog/model.scala
+++ src/main/scala/com/artkostm/posters/dialog/model.scala
@@ -1,5 +1,24 @@
 package com.artkostm.posters.dialog
 
-class model {
+import org.joda.time.DateTime
 
-}
+case class Datetime(date: Option[DateTime], period: Option[String])
+
+case class Parameters(category: List[String], datetime: Datetime)
+
+case class ContextParams(datetime: String, `datetime.original`: String, category: List[String], `category.original`: String)
+
+case class Context(name: String, parameters: ContextParams, lifespan: Int)
+
+case class Metadata(intentId: String, webhookUsed: String, webhookForSlotFillingUsed: String, intentName: String)
+
+case class Message(`type`: Int, speech: String)
+
+case class Fulfillment(speech: String, messages: List[Message])
+
+case class Result(source: String, resolvedQuery: String, speech: String, action: String, actionIncomplete: Boolean,
+                  parameters: Parameters, contexts: List[Context], metadata: Metadata, fulfillment: Fulfillment, score: Double)
+
+case class Status(code: Int, errorType: String, webhookTimedOut: Boolean)
+
+case class DialogflowRequest(id: String, lang: String, result: Result, status: Status, sessionId: String)
