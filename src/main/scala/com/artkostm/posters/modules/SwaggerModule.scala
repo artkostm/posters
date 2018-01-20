5eef8e1218393ec48b19dc8816c2d3e693ed7e12
Artsiom <artkostm@gmail.com>
2018-01-20 19:08:06 +0300

    swagger module

--- src/main/scala/com/artkostm/posters/modules/SwaggerModule.scala
+++ src/main/scala/com/artkostm/posters/modules/SwaggerModule.scala
@@ -1,5 +1,31 @@
 package com.artkostm.posters.modules
 
-object SwaggerModule {
+import javax.inject.Singleton
 
+import com.google.inject.Provides
+import com.jakehschwartz.finatra.swagger.SwaggerModule
+import io.swagger.models.auth.BasicAuthDefinition
+import io.swagger.models.{Info, Swagger}
+
+object PostersSwaggerModule extends SwaggerModule {
+  @Singleton
+  @Provides
+  def swagger: Swagger = {
+    val swagger = new Swagger()
+
+    val info = new Info()
+      .description("The Student / Course management API, this is a sample for swagger document generation")
+      .version("1.0.1")
+      .title("Posters / Events Retrieve API")
+
+    swagger
+      .info(info)
+      .addSecurityDefinition("sampleBasic", {
+        val d = new BasicAuthDefinition()
+        d.setType("basic")
+        d
+      })
+
+    swagger
+  }
 }
