a12cc29bd9b94c1ecea460e3599709f44fd0cf1b
Artsiom <artkostm@gmail.com>
2018-01-20 20:43:13 +0300

    update category controller

--- src/main/scala/com/artkostm/posters/controllers/CategoryController.scala
+++ src/main/scala/com/artkostm/posters/controllers/CategoryController.scala
@@ -1,5 +1,25 @@
 package com.artkostm.posters.controllers
 
-class CategoryController {
+import com.google.inject.Inject
+import com.jakehschwartz.finatra.swagger.SwaggerController
+import com.twitter.finatra.request.{QueryParam, RouteParam}
+import io.swagger.models.Swagger
+import org.joda.time.DateTime
 
+class CategoryController @Inject()(s: Swagger) extends SwaggerController
+  with AllDayOfEventsOperation
+  with EventsByCategoryNameOperation {
+
+  override implicit protected val swagger = s
+
+  getWithDoc("/posters/:date/?")(allDayOfEventsOp) { request: WithDate =>
+    s"only date: ${request.date}"
+  }
+
+  getWithDoc("/posters/?")(eventsByCategoryNameOp) { request: WithNameAndDate =>
+    s"Hello, ${request.name} Кино"
+  }
 }
+
+case class WithDate(@RouteParam date: DateTime)
+case class WithNameAndDate(@QueryParam name: String, @QueryParam date: DateTime)
