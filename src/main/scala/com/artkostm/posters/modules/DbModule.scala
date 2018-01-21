d8fd66ea0ad4b3c9647139a90caecbac85da6d28
Artsiom <artkostm@gmail.com>
2018-01-21 19:48:32 +0300

    db module

--- src/main/scala/com/artkostm/posters/modules/DbModule.scala
+++ src/main/scala/com/artkostm/posters/modules/DbModule.scala
@@ -1,18 +1,49 @@
 package com.artkostm.posters.modules
 
-import com.artkostm.posters._
-import com.artkostm.posters.repository.PostgresPostersRepository
+import java.net.URI
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+import javax.sql.DataSource
+
+import akka.actor.ActorSystem
+import com.artkostm.posters.repository.{PostersRepository, PostgresPostersRepository}
+import com.google.inject.{Module, Provides}
 import com.twitter.inject.{Injector, TwitterModule}
+import com.typesafe.config.Config
+import com.zaxxer.hikari.{HikariConfig, HikariDataSource}
+
+import scala.concurrent.duration.FiniteDuration
 
 object DbModule extends TwitterModule {
-  implicit val ec = actorSystem.dispatcher
+  override protected[inject] def modules: Seq[Module] = Seq(ConfigModule, AkkaModule, ToolsModule)
+
+  @Singleton @Provides def postgresDataSource(config: Config): DataSource = {
+    val uri = new URI(config.getString("postgres.url"))
+    val user = uri.getUserInfo().split(":")(0)
+    val password = uri.getUserInfo().split(":")(1)
+    val c = new HikariConfig()
+    c.setJdbcUrl(s"jdbc:postgresql://${uri.getHost()}:${uri.getPort()}${uri.getPath()}?sslmode=require")
+    c.setUsername(user)
+    c.setPassword(password)
+    c.setMaximumPoolSize(1)
+    c.addDataSourceProperty("sslmode", "require")
+    c.setConnectionTimeout(120000)
+    new HikariDataSource(c)
+  }
+
+  @Singleton @Provides def postersRepository(ds: DataSource): PostgresPostersRepository = new PostersRepository(ds)
 
   override def singletonStartup(injector: Injector): Unit = {
-    PostgresPostersRepository.setUp
-    eventsCollector.run()
+    val system = injector.instance[ActorSystem]
+    implicit val ec = system.dispatcher
+    //injector.instance[PostgresPostersRepository].setUp
+    system.scheduler.schedule(FiniteDuration(10, TimeUnit.SECONDS), FiniteDuration(24, TimeUnit.HOURS)) {
+      //injector.instance[EventsCollector].run()
+    }
   }
 
   override def singletonShutdown(injector: Injector): Unit = {
-    PostgresPostersRepository.db.close
+    implicit val ec = injector.instance[ActorSystem].dispatcher
+    injector.instance[PostgresPostersRepository].db.close
   }
 }
