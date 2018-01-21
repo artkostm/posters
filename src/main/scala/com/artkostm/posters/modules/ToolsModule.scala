02573012a18ce1f33e087793e335911e95458217
Artsiom <artkostm@gmail.com>
2018-01-21 19:48:24 +0300

    tools module

--- src/main/scala/com/artkostm/posters/modules/ToolsModule.scala
+++ src/main/scala/com/artkostm/posters/modules/ToolsModule.scala
@@ -1,5 +1,19 @@
 package com.artkostm.posters.modules
 
-class ToolsModule {
+import javax.inject.Singleton
 
+import com.artkostm.posters.collector.EventsCollector
+import com.artkostm.posters.repository.PostgresPostersRepository
+import com.artkostm.posters.scraper.EventsScraper
+import com.google.inject.{Module, Provides}
+import com.twitter.inject.TwitterModule
+import org.joda.time.DateTime
+
+object ToolsModule extends TwitterModule {
+  override protected[inject] def modules: Seq[Module] = Seq(ConfigModule, AkkaModule, DbModule)
+
+  @Singleton @Provides def scraper(scraperConfig: ScraperConfig): EventsScraper = new EventsScraper(scraperConfig)
+
+  @Singleton @Provides def collector(scraper: EventsScraper, repo: PostgresPostersRepository): EventsCollector =
+    new EventsCollector(scraper, (-2 to 30).map(DateTime.now().plusDays).map(_.withTimeAtStartOfDay()), repo)
 }
