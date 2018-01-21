2296ae53c96f395618a85828aa834d89ffeaf0cd
Artsiom <artkostm@gmail.com>
2018-01-21 19:48:48 +0300

    akka module

--- src/main/scala/com/artkostm/posters/modules/AkkaModule.scala
+++ src/main/scala/com/artkostm/posters/modules/AkkaModule.scala
@@ -1,23 +1,31 @@
 package com.artkostm.posters.modules
 
+import javax.inject.Singleton
+
 import akka.actor.SupervisorStrategy.Stop
-import akka.actor.{Actor, ActorRef, Props, Terminated}
-import com.artkostm.posters.actorSystem
+import akka.actor.{Actor, ActorRef, ActorSystem, Props, Terminated}
+import com.google.inject.Provides
 import com.twitter.inject.{Injector, TwitterModule}
 
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContext, Future}
 
 object AkkaModule extends TwitterModule {
-  private val actor: ActorRef = actorSystem.actorOf(Props[ShutdownActor])
+  private var actor: ActorRef = _
 
-  override def singletonStartup(injector: Injector): Unit = Runtime.getRuntime.addShutdownHook(new Thread(() => {
-      val terminate: Future[Terminated] = actorSystem.terminate()
+  @Singleton @Provides def actorSystem() = null
+
+  override def singletonStartup(injector: Injector): Unit = {
+    actor = injector.instance[ActorSystem].actorOf(Props[ShutdownActor])
+    Runtime.getRuntime.addShutdownHook(new Thread(() => {
+      val terminate: Future[Terminated] = injector.instance[ActorSystem].terminate()
       Await.result(terminate, Duration("10 seconds"))
-    }
-  ))
+    }))
+  }
 
-  override def singletonShutdown(injector: Injector): Unit = actor ! Stop
+  override def singletonShutdown(injector: Injector): Unit = {
+    actor ! Stop
+  }
 }
 
 private[modules] class ShutdownActor extends Actor {
