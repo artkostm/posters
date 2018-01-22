c3f3e501f42da71bcb439e650d8ca558c922b780
Artsiom <artkostm@gmail.com>
2018-01-22 22:02:47 +0300

    intents controller implementation

--- src/main/scala/com/artkostm/posters/controllers/IntentsController.scala
+++ src/main/scala/com/artkostm/posters/controllers/IntentsController.scala
@@ -1,5 +1,30 @@
 package com.artkostm.posters.controllers
 
-class IntentsController {
+import akka.actor.ActorSystem
+import com.artkostm.posters.model.Assign
+import com.artkostm.posters.repository.PostgresPostersRepository
+import com.google.inject.Inject
+import com.jakehschwartz.finatra.swagger.SwaggerController
+import com.twitter.finatra.request.QueryParam
+import io.swagger.models.Swagger
+import org.joda.time.DateTime
 
+class IntentsController @Inject() (s: Swagger, repository: PostgresPostersRepository,
+                                   system: ActorSystem)
+  extends SwaggerController with GetIntentOperation with SaveIntentOperation {
+  override implicit protected val swagger = s
+
+  private implicit val ec = system.dispatcher
+
+  postWithDoc("/intents/?")(saveIntentOp) { request: Assign =>
+    repository.save(request)
+  }
+
+  getWithDoc("/intents/?")(getIntentOp) { request: AssigneeRequest =>
+    repository.find(request.category, request.date, request.name)
+  }
 }
+
+case class AssigneeRequest(@QueryParam date: DateTime,
+                           @QueryParam category: String,
+                           @QueryParam name: String)
