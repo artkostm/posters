79d0a975e20f88abdad2e342153d1c1a0b91bce8
Artsiom <artkostm@gmail.com>
2018-01-22 22:03:09 +0300

    events info controller

--- src/main/scala/com/artkostm/posters/controllers/EventInfoController.scala
+++ src/main/scala/com/artkostm/posters/controllers/EventInfoController.scala
@@ -1,5 +1,22 @@
 package com.artkostm.posters.controllers
 
-class EventInfoController {
+import akka.actor.ActorSystem
+import com.artkostm.posters.repository.PostgresPostersRepository
+import com.google.inject.Inject
+import com.jakehschwartz.finatra.swagger.SwaggerController
+import com.twitter.finatra.request.QueryParam
+import io.swagger.models.Swagger
 
+class EventInfoController @Inject() (s: Swagger, repository: PostgresPostersRepository,
+                                     system: ActorSystem)
+  extends SwaggerController with EventInfoByLinkOperation {
+  override implicit protected val swagger = s
+
+  private implicit val ec = system.dispatcher
+
+  getWithDoc("/info/?")(eventInfoByLinkOp) { request: EventInfoRequest =>
+    repository.find(request.link)
+  }
 }
+
+case class EventInfoRequest(@QueryParam link: String)
