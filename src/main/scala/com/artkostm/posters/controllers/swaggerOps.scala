4b829c4d9b7fbf6d92fb14d7c8a1d9cd194a9444
Artsiom <artkostm@gmail.com>
2018-01-26 22:05:33 +0300

    add operations

--- src/main/scala/com/artkostm/posters/controllers/swaggerOps.scala
+++ src/main/scala/com/artkostm/posters/controllers/swaggerOps.scala
@@ -17,9 +17,7 @@ trait AllDayOfEventsOperation { self: SwaggerController =>
           Event(Media(
             "event link", "image link"), "event name", Description("event description", Some("ticket link"), true)))))))
       .responseWith(404, "categories are not found for received date")
-}
 
-trait EventsByCategoryNameOperation { self: SwaggerController =>
   def eventsByCategoryNameOp(o: Operation): Operation =
     o.summary("Get all events by category name for one day")
       .description("Get all events by category name for one day")
@@ -47,8 +45,18 @@ trait EventInfoByLinkOperation { self: SwaggerController =>
       .responseWith(404, "event is not found")
 }
 
-trait GetIntentOperation { self: SwaggerController =>
+trait IntentOperation { self: SwaggerController =>
   def getIntentOp(o: Operation): Operation =
+    o.summary("Get an event using direct link to it")
+      .description("Get an event busing direct link to it.")
+      .tag("Event")
+      .queryParam[String]("link", "the event link in the format https://.../")
+      .produces("application/json")
+      .responseWith[Info](200, "singleton list of an category found", example = Some(Info("https://link.to/the/specified/event/",
+      EventInfo("event detailed description", List("https://links.to/the/event/photo/"), List(Comment("author", "date", "comment text"))))))
+      .responseWith(404, "event is not found")
+
+  def saveIntentOp(o: Operation): Operation =
     o.summary("Get an event using direct link to it")
       .description("Get an event busing direct link to it.")
       .tag("Event")
@@ -59,11 +67,11 @@ trait GetIntentOperation { self: SwaggerController =>
       .responseWith(404, "event is not found")
 }
 
-trait SaveIntentOperation { self: SwaggerController =>
-  def saveIntentOp(o: Operation): Operation =
-    o.summary("Get an event using direct link to it")
-      .description("Get an event busing direct link to it.")
-      .tag("Event")
+trait DialogflowWebhookOperation { self: SwaggerController =>
+  def webhookOp(o: Operation): Operation =
+    o.summary("Get all events by category name for the requested date")
+      .description("Get all events by category name for the requested date.")
+      .tag("Dialogflow")
       .queryParam[String]("link", "the event link in the format https://.../")
       .produces("application/json")
       .responseWith[Info](200, "singleton list of an category found", example = Some(Info("https://link.to/the/specified/event/",
