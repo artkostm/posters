1655ee531ae60b6825964fb4aef9a831291d28fd
Artsiom <artkostm@gmail.com>
2018-01-26 22:06:04 +0300

    dialogflow webhook

--- src/main/scala/com/artkostm/posters/controllers/DialogflowWebhook.scala
+++ src/main/scala/com/artkostm/posters/controllers/DialogflowWebhook.scala
@@ -1,5 +1,41 @@
 package com.artkostm.posters.controllers
 
-class DialogflowWebhook {
+import akka.actor.ActorSystem
+import com.artkostm.posters.ErrorResponse
+import com.artkostm.posters.dialog._
+import com.artkostm.posters.repository.PostgresPostersRepository
+import com.artkostm.posters.scraper.EventsScraper
+import com.google.inject.Inject
+import com.jakehschwartz.finatra.swagger.SwaggerController
+import io.swagger.models.Swagger
 
+import scala.concurrent.Future
+
+class DialogflowWebhook @Inject() (s: Swagger, repository: PostgresPostersRepository,
+                                   system: ActorSystem, scraper: EventsScraper)
+  extends SwaggerController with DialogflowWebhookOperation {
+  override implicit protected val swagger = s
+
+  private implicit val ec = system.dispatcher
+
+  postWithDoc("/webhook/?")(webhookOp) { request: DialogflowRequest =>
+    if (!FlowKeyDataExtractor.actionIncomplete(request))
+      FlowKeyDataExtractor.extract(request) match {
+        case FlowKeyData(categories, Some(date), _) =>
+          if (FlowKeyDataExtractor.shouldShowAll(request)) repository.find(date).map {
+            case Some(day) =>
+              DialogflowResponse("", ResponseData(day.categories), "posters")
+            case None =>
+              DialogflowResponse("", ResponseData(scraper.scheduleFor(date).events), "posters")
+          } else repository.findCategories(date, categories).map {
+            case IndexedSeq() =>
+              DialogflowResponse("", ResponseData(scraper.scheduleFor(date).events.filter(cat => categories.contains(cat.name))), "posters")
+            case nonEmpty =>
+              DialogflowResponse("", ResponseData(nonEmpty.toList), "posters")
+          }
+        case FlowKeyData(category, _, Some(period)) => Future.failed(???)
+        case _ => Future.successful(response.badRequest(ErrorResponse("1", "cannot extract key data")))
+      }
+    else Future.successful(response.badRequest(ErrorResponse("1", "action is incomplete")))
+  }
 }
