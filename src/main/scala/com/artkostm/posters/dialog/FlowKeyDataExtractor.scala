37749d06d8c172330b4ecc8eaf86af3c183a7708
Artsiom <artkostm@gmail.com>
2018-01-28 18:06:24 +0300

    add period parser

--- src/main/scala/com/artkostm/posters/dialog/FlowKeyDataExtractor.scala
+++ src/main/scala/com/artkostm/posters/dialog/FlowKeyDataExtractor.scala
@@ -1,6 +1,6 @@
 package com.artkostm.posters.dialog
 
-import org.joda.time.DateTime
+import org.joda.time.{DateTime, Days}
 
 case class FlowKeyData(category: List[String], date: Option[DateTime], period: Option[String])
 
@@ -12,6 +12,17 @@ trait FlowKeyDataExtractor {
     val date = request.result.parameters.datetime.date
     val period = request.result.parameters.datetime.period
     FlowKeyData(category, date, period)
+  }
+
+  // period in the format yyyy-mm-dd/yyyy-mm-dd
+  def getPeriod(period: String): List[DateTime] = {
+    period.split("/") match {
+      case Array(start, end) =>
+        val startDate = DateTime.parse(start)
+        val endDate = DateTime.parse(end)
+        (0 to Days.daysBetween(startDate, endDate).getDays).map(startDate.plusDays(_)).toList
+      case _ => throw new IllegalArgumentException ("Invalid period format")
+    }
   }
 }
 
