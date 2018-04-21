85995c85c728a1e56818242501540d9854180904
Artsiom <artkostm@gmail.com>
2018-04-21 16:43:19 +0300

    v2 rq/rs models

--- src/main/scala/com/artkostm/posters/dialog/v2/DfModel.scala
+++ src/main/scala/com/artkostm/posters/dialog/v2/DfModel.scala
@@ -1,5 +1,18 @@
 package com.artkostm.posters.dialog.v2
 
-class DfModel {
+import com.artkostm.posters.model.Category
+import org.joda.time.DateTime
 
-}
+case class Datetime(date: Option[DateTime], period: Option[String])
+case class Parameters(category: List[String], datetime: Datetime)
+case class Intent(name: String, displayName: String)
+case class DiagnosticInfo()
+case class QueryResult(queryText: String, allRequiredParamsPresent: Boolean, intent: Intent, intentDetectionConfidence: Double,
+                       diagnosticInfo: DiagnosticInfo, languageCode: String)
+case class Payload()
+case class OriginalDetectIntentRequest(payload: Payload)
+case class DislogFlowRequest(responseId: String, originalDetectIntentRequest: OriginalDetectIntentRequest, session: String)
+
+
+case class ResponsePayload(categories: List[Category])
+case class DialogflowResponse(fulfillmentText: String, payload: ResponsePayload, source: String)
