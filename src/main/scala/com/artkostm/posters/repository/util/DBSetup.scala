bb77a017007c8663e34aa24371756c4c41f9acf1
Artsiom <artkostm@gmail.com>
2018-04-27 23:04:23 +0300

    small update

--- src/main/scala/com/artkostm/posters/repository/util/DBSetup.scala
+++ src/main/scala/com/artkostm/posters/repository/util/DBSetup.scala
@@ -1,7 +1,7 @@
 package com.artkostm.posters.repository.util
 
 import com.artkostm.posters.repository.{HasDatabaseConfig, PostersPgProfile}
-import slick.dbio.{DBIOAction, DatabaseAction}
+import slick.dbio.{DBIOAction => DA}
 import slick.jdbc.meta.MTable
 
 import scala.concurrent.ExecutionContext
@@ -9,11 +9,12 @@ import scala.concurrent.ExecutionContext
 trait DBSetupOps { self: HasDatabaseConfig[PostersPgProfile] =>
   import profile.api._
 
-  def setUp[E<: Table[_]](query: TableQuery[E], deleteAction: DatabaseAction[Int, NoStream, Effect.Write])(implicit ctx: ExecutionContext) = {
+  def setUp[E<: Table[_], R, S <: slick.dbio.NoStream, F <: slick.dbio.Effect](query: TableQuery[E], deleteAction: DBIOAction[R, S, F])
+                         (implicit ctx: ExecutionContext) = {
     val createIfNotExist = MTable.getTables.flatMap(v => {
       val names = v.map(_.name.name)
       if (!names.contains(query.baseTableRow.tableName)) query.schema.create
-      else DBIOAction.successful()
+      else DA.successful()
     })
     DBIO.seq(createIfNotExist, deleteAction)
   }
