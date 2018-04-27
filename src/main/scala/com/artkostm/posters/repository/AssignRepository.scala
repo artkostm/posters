55d5600af7491a5f148a4da33d29dc9adce739bc
Artsiom <artkostm@gmail.com>
2018-04-27 22:00:15 +0300

    update assign repo

--- src/main/scala/com/artkostm/posters/repository/AssignRepository.scala
+++ src/main/scala/com/artkostm/posters/repository/AssignRepository.scala
@@ -1,33 +1,47 @@
 package com.artkostm.posters.repository
 
-import com.artkostm.posters.model.{Assign, AssignTable}
+import com.artkostm.posters.model.Assign
+import com.artkostm.posters.repository.util.DBSetupOps
 import org.joda.time.DateTime
-import slick.dbio.DBIOAction
-import slick.jdbc.meta.MTable
+import slick.lifted.ProvenShape
 
 import scala.concurrent.{ExecutionContext, Future}
 
-trait AssignRepository extends AssignTable { this: DbComponent =>
-  import driver.api._
+trait AssignTable { self: HasDatabaseConfig[PostersPgProfile] =>
+  import profile.api._
 
-  def setUpAssign()(implicit ctx: ExecutionContext) = {
-    val createIfNotExist = MTable.getTables.flatMap(v => {
-      val names = v.map(_.name.name)
-      if (!names.contains(assigneesTableQuery.baseTableRow.tableName)) assigneesTableQuery.schema.create
-      else DBIOAction.successful()
-    })
-    DBIO.seq(createIfNotExist, assigneesTableQuery.filter(event => event.date < DateTime.now.minusDays(1)).delete)
+  private[AssignTable] class Assignees(tag: Tag) extends Table[Assign](tag, "assignees") {
+
+    def category: Rep[String] = column[String]("category")
+    def date: Rep[DateTime] = column[DateTime]("date")
+    def eventName: Rep[String] = column[String]("event_name")
+    def ids: Rep[String] = column[String]("ids")
+
+    def * : ProvenShape[Assign] = (category, date, eventName, ids) <> (Assign.tupled, Assign.unapply)
+    def pk = primaryKey("pk_events", (category, date, eventName))
   }
 
+  protected val Assignees = TableQuery[Assignees]
+}
+
+
+trait AssignRepository extends AssignTable with DBSetupOps { self: HasDatabaseConfig[PostersPgProfile] =>
+  import profile.api._
+
+  def setUpAssign()(implicit ctx: ExecutionContext) =
+    setUp(Assignees, Assignees.filter(event => event.date < DateTime.now.minusDays(1)).delete)
+
   def save(assign: Assign) = db.run(
-    assigneesTableQuery.insertOrUpdate(assign.copy(date = assign.date.withTimeAtStartOfDay())).transactionally
+    Assignees.insertOrUpdate(assign.copy(date = assign.date.withTimeAtStartOfDay())).transactionally
   )
 
-  def all: Future[List[Assign]] = db.run(assigneesTableQuery.to[List].result)
+  def all: Future[List[Assign]] = db.run(Assignees.to[List].result)
 
   def find(category: String, date: DateTime, eventName: String): Future[Option[Assign]] = db.run {
-    assigneesTableQuery.filter(a => a.category === category &&
-      a.eventName === eventName &&
-      a.date === date.withTimeAtStartOfDay()).result.headOption
+    Assignees.filter(a =>
+      a.category === category
+        && a.eventName === eventName
+        && a.date === date.withTimeAtStartOfDay()
+    ).result.headOption
   }
 }
