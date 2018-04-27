0d52eb13370673fb40fe38517405e5717660b6d3
Artsiom <artkostm@gmail.com>
2018-04-27 23:04:10 +0300

    refactoring

--- src/main/scala/com/artkostm/posters/repository/DbComponents.scala
+++ src/main/scala/com/artkostm/posters/repository/DbComponents.scala
@@ -1,39 +1,12 @@
 package com.artkostm.posters.repository
 
-import javax.sql.DataSource
+import slick.jdbc.JdbcProfile
 
-import com.google.inject.Inject
-
-import scala.concurrent.{ExecutionContext, Future}
-
-trait H2DbComponent extends DbComponent {
-  override lazy val driver = slick.jdbc.H2Profile
+trait DbComponent {
+  protected val driver: JdbcProfile
 
   import driver.api._
 
-  override lazy val db: driver.api.Database = Database.forConfig("h2mem1")
+  val db: Database
 }
 
-trait JsonSupportDbComponent extends DbComponent {
-  protected def dataSource: DataSource
-  override lazy val driver = JsonSupportPostgresProfile
-
-  import driver.api._
-
-  override lazy val db: driver.api.Database = Database.forDataSource(dataSource, Some(19))
-}
-
-trait PostgresPostersRepository extends AssignRepository
-                                    with DaysRepository
-                                    with InfoRepository
-                                    with JsonSupportDbComponent {
-  import driver.api._
-  def setUp()(implicit ec: ExecutionContext) =
-    db.run((setUpAssign >> setUpDays >> setUpInfo >> sqlu"""
-            DELETE FROM info WHERE NOT EXISTS (SELECT * FROM days WHERE categories::jsonb::text LIKE '%' || info.link || '%')
-          """).transactionally)
-}
-
-class PostersRepository @Inject() (ds: DataSource) extends PostgresPostersRepository {
-  override protected def dataSource: DataSource = ds
-}
---
+++ src/main/scala/com/artkostm/posters/repository/PostersRepository.scala
@@ -0,0 +1,32 @@
+package com.artkostm.posters.repository
+
+import javax.sql.DataSource
+
+import com.google.inject.Inject
+
+import scala.concurrent.ExecutionContext
+
+trait JsonSupportDbComponent extends DbComponent {
+  protected def dataSource: DataSource
+  override lazy val driver = JsonSupportPostgresProfile
+
+  import driver.api._
+
+  override lazy val db: driver.api.Database = Database.forDataSource(dataSource, Some(19))
+}
+
+trait PostgresPostersRepository extends AssignRepository
+                                    with DaysRepository
+                                    with InfoRepository
+                                    with TestRepo
+                                    with JsonSupportDbComponent {
+  import driver.api._
+  def setUp()(implicit ec: ExecutionContext) =
+    db.run((setUpAssign >> setUpDays >> setUpInfo >> sqlu"""
+            DELETE FROM info WHERE NOT EXISTS (SELECT * FROM days WHERE categories::jsonb::text LIKE '%' || info.link || '%')
+          """).transactionally)
+}
+
+class PostersRepository @Inject() (ds: DataSource) extends PostgresPostersRepository {
+  override protected def dataSource: DataSource = ds
+}
