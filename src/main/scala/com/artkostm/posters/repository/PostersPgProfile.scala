cc52c40c21db787a1856d77be9be6cbeb4526546
Artsiom <artkostm@gmail.com>
2018-04-27 23:04:43 +0300

    update profile

--- src/main/scala/com/artkostm/posters/repository/PostersPgProfile.scala
+++ src/main/scala/com/artkostm/posters/repository/PostersPgProfile.scala
@@ -1,7 +1,10 @@
 package com.artkostm.posters.repository
 
+import com.artkostm.posters.model.{Category, EventInfo}
+import com.github.tminglei.slickpg.utils.SimpleArrayUtils
 import com.github.tminglei.slickpg.{ExPostgresProfile, PgArraySupport, PgDate2Support, PgPlayJsonSupport}
 import org.joda.time.DateTime
+import play.api.libs.json.{JsValue, Json}
 
 trait PostersPgProfile extends ExPostgresProfile
   with PgArraySupport
@@ -20,6 +23,19 @@ trait PostersPgProfile extends ExPostgresProfile
       sqlTimestamp => new DateTime(sqlTimestamp.getTime()))
 
     implicit val strListTypeMapper = new SimpleArrayJdbcType[String]("text").to(_.toList)
+
+    import Category._
+    import EventInfo._
+
+    implicit val categoryJsonTypeMapper = MappedJdbcType.base[Category, JsValue](Json.toJson(_), _.as[Category])
+
+    implicit val eventInfoJsonTypeMapper = MappedJdbcType.base[EventInfo, JsValue](Json.toJson(_), _.as[EventInfo])
+
+    implicit val categoryArrayTypeMapper = MappedJdbcType.base[List[Category], JsValue](Json.toJson(_), _.as[List[Category]])
+
+    implicit val eventInfoArrayTypeMapper = new AdvancedArrayJdbcType[EventInfo](pgjson,
+      (s) => SimpleArrayUtils.fromString[EventInfo](Json.parse(_).as[EventInfo])(s).orNull,
+      (v) => SimpleArrayUtils.mkString[EventInfo](c => Json.toJson(c).toString)(v)).to(_.toList)
   }
 }
 
