a0de7ad8d4ee32318a8a902cddc3d58ccc67575e
Artsiom <artkostm@gmail.com>
2018-04-27 23:04:54 +0300

    info repository

--- src/main/scala/com/artkostm/posters/repository/InfoRepository.scala
+++ src/main/scala/com/artkostm/posters/repository/InfoRepository.scala
@@ -1,26 +1,35 @@
 package com.artkostm.posters.repository
 
-import com.artkostm.posters.model.{Info, InfoTable}
+import com.artkostm.posters.model.{EventInfo, Info}
+import com.artkostm.posters.repository.util.DBSetupOps
 import slick.dbio.DBIOAction
-import slick.jdbc.meta.MTable
+import slick.lifted.ProvenShape
 
 import scala.concurrent.{ExecutionContext, Future}
 
-trait InfoRepository extends InfoTable { this: JsonSupportDbComponent =>
-  import driver.plainAPI._
+trait InfoTable { self: HasDatabaseConfig[PostersPgProfile] =>
+  import profile.api._
 
-  def setUpInfo()(implicit ctx: ExecutionContext) = {
-    val createIfNotExist = MTable.getTables.flatMap(v => {
-      val names = v.map(_.name.name)
-      if (!names.contains(infoTableQuery.baseTableRow.tableName)) infoTableQuery.schema.create
-      else DBIOAction.successful()
-    })
-    DBIO.seq(createIfNotExist)
+  private[InfoTable] class Information(tag: Tag) extends Table[Info](tag, "info") {
+    def link: Rep[String] = column[String]("link")
+    def eventsInfo: Rep[EventInfo] = column[EventInfo]("eventsInfo")
+
+    def * : ProvenShape[Info] = (link, eventsInfo) <> (Info.tupled, Info.unapply)
+    def pk = primaryKey("pk_info", link)
   }
 
-  def save(info: Info): Future[Int] = db.run(infoTableQuery.insertOrUpdate(info))
+  protected val Info = TableQuery[Information]
+}
+
+trait InfoRepository extends InfoTable with DBSetupOps { self: HasDatabaseConfig[PostersPgProfile] =>
+  import profile.api._
+
+  def setUpInfo()(implicit ctx: ExecutionContext) =
+    setUp(Info, DBIOAction.successful())
+
+  def save(info: Info): Future[Int] = db.run(Info.insertOrUpdate(info))
 
   def find(link: String): Future[Option[Info]] = db.run {
-    infoTableQuery.filter(i => i.link === link).result.headOption
+    Info.filter(i => i.link === link).result.headOption
   }
 }
