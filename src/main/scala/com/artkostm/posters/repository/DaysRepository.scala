ddace72eb1374245ab65d1fcfc80d6f6b983feb7
Artsiom <artkostm@gmail.com>
2018-04-27 23:05:18 +0300

    update days repo

--- src/main/scala/com/artkostm/posters/repository/DaysRepository.scala
+++ src/main/scala/com/artkostm/posters/repository/DaysRepository.scala
@@ -1,33 +1,39 @@
 package com.artkostm.posters.repository
 
-import com.artkostm.posters.model.{Category, EventsDay, EventsDayTable}
+import com.artkostm.posters.model._
+import com.artkostm.posters.repository.util.DBSetupOps
 import org.joda.time.DateTime
 import play.api.libs.json.Json
-import slick.dbio.DBIOAction
 import slick.jdbc.{GetResult, PositionedParameters, SQLActionBuilder, SetParameter}
-import slick.jdbc.meta.MTable
+import slick.lifted.ProvenShape
 
 import scala.concurrent.{ExecutionContext, Future}
 
-trait DaysRepository extends EventsDayTable {
-  this: JsonSupportDbComponent =>
+trait EventsDayTable { self: HasDatabaseConfig[PostersPgProfile] =>
+  import profile.api._
 
-  import driver.plainAPI._
+  private[EventsDayTable] class Days(tag: Tag) extends Table[EventsDay](tag, "days") {
+    def date: Rep[DateTime] = column[DateTime]("date")
+    def categories: Rep[List[Category]] = column[List[Category]]("categories")
 
-  def setUpDays()(implicit ctx: ExecutionContext) = {
-    val createIfNotExist = MTable.getTables.flatMap(v => {
-      val names = v.map(_.name.name)
-      if (!names.contains(daysTableQuery.baseTableRow.tableName)) daysTableQuery.schema.create
-      else DBIOAction.successful()
-    })
-    DBIO.seq(createIfNotExist, daysTableQuery.filter(day => day.date < DateTime.now.minusDays(1)).delete)
+    def * : ProvenShape[EventsDay] = (date, categories) <> (EventsDay.tupled, EventsDay.unapply)
+    def pk = primaryKey("pk_days", date)
   }
 
-  def save(day: EventsDay): Future[Int] = db.run(daysTableQuery.insertOrUpdate(day.copy(date = day.date.withTimeAtStartOfDay())))
+  protected val Days = TableQuery[Days]
+}
 
-  def find(date: DateTime): Future[Option[EventsDay]] = db.run {
-    daysTableQuery.filter(d => d.date === date.withTimeAtStartOfDay()).result.headOption
-  }
+trait DaysRepository extends EventsDayTable with DBSetupOps { self: HasDatabaseConfig[PostersPgProfile] =>
+
+  import profile.api._
+
+  def setUpDays()(implicit ctx: ExecutionContext) =
+    setUp(Days, Days.filter(day => day.date < DateTime.now.minusDays(1)).delete)
+
+  def save(day: EventsDay): Future[Int] = db.run(Days.insertOrUpdate(day.copy(date = day.date.withTimeAtStartOfDay())))
+
+  def find(date: DateTime): Future[Option[EventsDay]] =
+    db.run(Days.filter(d => d.date === date.withTimeAtStartOfDay()).result.headOption)
 
   import Category._
 
@@ -68,4 +74,22 @@ trait DaysRepository extends EventsDayTable {
     concat(sql"""
          SELECT obj FROM days d, jsonb_array_elements(d.categories) obj WHERE date >= $startDate AND date <= $endDate
        """, concat(sql""" AND obj->>'name' IN """, values(names))).as[Category])
+}
+
+trait TestRepo extends TestTable {
+  this: JsonSupportDbComponent =>
+
+  import driver.api._
+
+  def test(names: List[String]) = {
+
+    val q = for {
+      data <- testQuery.map(_.data.arrayElements)
+      if (data.+>>("name")) inSet names
+    } yield data
+
+    q.result.statements.foreach(println)
+    null
+  }
+
 }
