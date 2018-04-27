300939e2f12f88f41345ef9b0ea2e575f9e6ff2d
Artsiom <artkostm@gmail.com>
2018-04-27 23:05:05 +0300

    update db components

--- src/main/scala/com/artkostm/posters/repository/DbComponents.scala
+++ src/main/scala/com/artkostm/posters/repository/DbComponents.scala
@@ -1,12 +1,27 @@
 package com.artkostm.posters.repository
 
-import slick.jdbc.JdbcProfile
+import javax.sql.DataSource
+import slick.basic.BasicProfile
 
-trait DbComponent {
-  protected val driver: JdbcProfile
+sealed trait DbComponent[P <: BasicProfile] {
+  protected[repository] val driver: P
 
-  import driver.api._
-
-  val db: Database
+  protected[repository] val db: P#Backend#Database
 }
 
+trait JsonSupportDbComponent extends DbComponent[PostersPgProfile] {
+  protected def dataSource: DataSource
+  protected override lazy val driver = PostersPgProfile
+
+  import driver.api._
+  override lazy val db = Database.forDataSource(dataSource, Some(19))
+}
+
+trait HasDatabaseConfig[P <: BasicProfile] {
+
+  protected val component: DbComponent[P]
+
+  protected final lazy val profile: P = component.driver
+
+  protected final def db: P#Backend#Database = component.db
+}
