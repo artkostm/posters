56188584b96a36101d371f24a12dea12fbeb23d2
Artsiom <artkostm@gmail.com>
2018-06-04 21:31:40 +0300

    auth filter

--- src/main/scala/com/artkostm/posters/controllers/auth/AuthFilter.scala
+++ src/main/scala/com/artkostm/posters/controllers/auth/AuthFilter.scala
@@ -1,5 +1,10 @@
 package com.artkostm.posters.controllers.auth
 
-class AuthFilter {
+import com.twitter.finagle.{Service, SimpleFilter}
+import com.twitter.finagle.http.{Request, Response}
+import com.twitter.util.Future
+import javax.inject.{Inject, Singleton}
 
+@Singleton class AuthFilter extends SimpleFilter[Request, Response]{
+  override def apply(request: Request, service: Service[Request, Response]): Future[Response] = service(request)
 }
