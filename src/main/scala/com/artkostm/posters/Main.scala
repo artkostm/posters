6d4549487676a2e51d225b45b33e2ee968aa77ef
Artsiom <artkostm@gmail.com>
2018-07-08 21:49:02 +0300

    extend example

--- src/main/scala/com/artkostm/posters/Main.scala
+++ src/main/scala/com/artkostm/posters/Main.scala
@@ -1,13 +1,21 @@
 package com.artkostm.posters
 
+import java.net.URI
+
 import eu.timepit.refined.api.Refined
 import eu.timepit.refined.string.MatchesRegex
 import eu.timepit.refined.W
+import cats.effect._
+import fs2.StreamApp
+import org.http4s._
+import org.http4s.dsl.io._
 
-object Main extends App {
+import scala.concurrent.ExecutionContext.Implicits.global
+
+object Main extends StreamApp[IO] {
   type ApiKey = String Refined MatchesRegex[W.`"[a-zA-Z0-9]{25,40}"`.T]
 
-  import ciris.{env, prop}
+  import ciris.env
 
   import ciris.refined._
 
@@ -15,4 +23,33 @@ object Main extends App {
     case Right(key) => s"Here is the key: $key"
     case _ => "Something went wrong"
   })
+
+  println(env[URI]("pg").value match {
+    case Right(url) => s"Here is the url: $url"
+    case _ => "Something went wrong"
+  })
+
+  val helloWorldService = HttpService[IO] {
+    case GET -> Root / "hello" / name =>
+      Ok(s"Hello, $name.")
+  }
+
+  import fs2.Stream
+
+  import fs2.StreamApp.ExitCode
+
+  import org.http4s.server.blaze._
+
+  override def stream(args: List[String], requestShutdown: IO[Unit]): Stream[IO, ExitCode] =
+    BlazeBuilder[IO]
+      .bindHttp(8080, "localhost")
+      .mountService(helloWorldService, "/")
+      .serve
+//  val builder = BlazeBuilder[IO].bindHttp(8080, "localhost").mountService(helloWorldService, "/").start
+//
+//  val server = builder.unsafeRunSync()
+//
+//  Thread.sleep(100000)
+//
+//  server.shutdown.unsafeRunSync()
 }//extends PostersServer
