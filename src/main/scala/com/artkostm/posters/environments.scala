e125ed106df37c7da5f37155036d91013460bbd9
Artsiom <artkostm@gmail.com>
2018-07-08 17:59:10 +0300

    envs

--- src/main/scala/com/artkostm/posters/environments.scala
+++ src/main/scala/com/artkostm/posters/environments.scala
@@ -1,5 +1,15 @@
 package com.artkostm.posters
 
 object environments {
+  import enumeratum._
+
+  sealed abstract class AppEnvironment extends EnumEntry
+  object AppEnvironment extends Enum[AppEnvironment] {
+    case object Local extends AppEnvironment
+    case object Testing extends AppEnvironment
+    case object Production extends AppEnvironment
+
+    val values = findValues
+  }
 
 }
