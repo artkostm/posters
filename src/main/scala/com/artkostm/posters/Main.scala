f0c88a0d336eaaa81d526c848adfea89a0abea81
Artsiom <artkostm@gmail.com>
2018-07-08 17:59:18 +0300

    add example

--- src/main/scala/com/artkostm/posters/Main.scala
+++ src/main/scala/com/artkostm/posters/Main.scala
@@ -1,3 +1,18 @@
 package com.artkostm.posters
 
-object Main extends PostersServer
+import eu.timepit.refined.api.Refined
+import eu.timepit.refined.string.MatchesRegex
+import eu.timepit.refined.W
+
+object Main extends App {
+  type ApiKey = String Refined MatchesRegex[W.`"[a-zA-Z0-9]{25,40}"`.T]
+
+  import ciris.{env, prop}
+
+  import ciris.refined._
+
+  println(env[ApiKey]("API_KEY").value match {
+    case Right(key) => s"Here is the key: $key"
+    case _ => "Something went wrong"
+  })
+}//extends PostersServer
