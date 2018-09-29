5ee3e00bee0114f37a5cbf9cd006b5a5caca6a24
Artsiom <artkostm@gmail.com>
2018-09-29 23:36:45 +0300

    add context

--- worker/src/main/scala/com/artkostm/posters/worker/Context.scala
+++ worker/src/main/scala/com/artkostm/posters/worker/Context.scala
@@ -1,5 +1,13 @@
 package com.artkostm.posters.worker
 
-class Context {
+import cats.effect.Async
+import fs2.Stream
 
+final case class Context[F[_]]()
+
+object Context {
+  def prepare[F[_]: Async]: Stream[F, Context[F]] =
+    for {
+      _ <- Stream.eval(DoobieMigration.run[F]())
+    } yield Context[F]()
 }
