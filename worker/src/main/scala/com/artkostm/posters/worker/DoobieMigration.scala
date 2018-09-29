e0244d979a761d6c6621d63cb63229999c088fd7
Artsiom <artkostm@gmail.com>
2018-09-29 23:36:35 +0300

    add doobie migration

--- worker/src/main/scala/com/artkostm/posters/worker/DoobieMigration.scala
+++ worker/src/main/scala/com/artkostm/posters/worker/DoobieMigration.scala
@@ -1,5 +1,29 @@
 package com.artkostm.posters.worker
 
-class DoobieMigration {
+import java.sql.Connection
 
+import cats.effect.{IO, Sync}
+import doobie._
+import doobie.implicits._
+import org.flywaydb.core.Flyway
+import org.flywaydb.core.api.migration.jdbc.JdbcMigration
+
+trait DoobieMigration extends JdbcMigration {
+  def migrate: ConnectionIO[_]
+
+  override def migrate(connection: Connection): Unit = {
+    val xa = Transactor.fromConnection[IO](connection)
+    migrate.transact(xa).unsafeRunSync()
+  }
+}
+
+object DoobieMigration {
+  def run[F[_]]()(implicit F: Sync[F]): F[Int] = F.delay {
+    val flyway = new Flyway
+    val location = classOf[migration.V0001__CreateVisitors].getPackage.getName.replace(".", "/")
+
+    flyway.setDataSource("jdbc:h2:~/test;MODE=PostgreSQL", "", "")
+    flyway.setLocations(location)
+    flyway.migrate()
+  }
 }
