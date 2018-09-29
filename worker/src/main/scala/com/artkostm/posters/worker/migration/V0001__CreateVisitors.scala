778665666259ab42799297b5ae76009631edcb61
Artsiom <artkostm@gmail.com>
2018-09-29 23:37:20 +0300

    create visitors migration

--- worker/src/main/scala/com/artkostm/posters/worker/migration/V0001__CreateVisitors.scala
+++ worker/src/main/scala/com/artkostm/posters/worker/migration/V0001__CreateVisitors.scala
@@ -1,5 +1,20 @@
 package com.artkostm.posters.worker.migration
 
-class V0001__CreateVisitors {
+import doobie._
+import doobie.implicits._
+import com.artkostm.posters.worker.DoobieMigration
 
+final class V0001__CreateVisitors extends DoobieMigration {
+  override def migrate: ConnectionIO[_] =
+    sql"""
+         CREATE TABLE IF NOT EXISTS visitors
+         (
+            date timestamp NOT NULL,
+            event_name varchar NOT NULL,
+            vids text NOT NULL,
+            uids text NOT NULL,
+            CONSTRAINT pk_visitors
+              PRIMARY KEY (date, event_name)
+         )
+    """.update.run
 }
