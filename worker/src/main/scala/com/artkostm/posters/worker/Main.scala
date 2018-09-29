ec4a7d706bd09cdc69018a9b501211c22c0f8c9d
Artsiom <artkostm@gmail.com>
2018-09-29 23:36:59 +0300

    update main

--- worker/src/main/scala/com/artkostm/posters/worker/Main.scala
+++ worker/src/main/scala/com/artkostm/posters/worker/Main.scala
@@ -1,5 +1,10 @@
 package com.artkostm.posters.worker
 
-class Main extends App {
+import cats.effect.IO
+import fs2.StreamApp.ExitCode
+import fs2.{Stream, StreamApp}
 
+object Main extends StreamApp[IO] {
+  override def stream(args: List[String], requestShutdown: IO[Unit]): Stream[IO, StreamApp.ExitCode] =
+    Context.prepare[IO].flatMap(_ => Stream.eval(IO.pure(ExitCode.Success)))
 }
