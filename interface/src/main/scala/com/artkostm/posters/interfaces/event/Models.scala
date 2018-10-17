6dc41d50cadddf2f57dc6addacec8074dccc8931
Artsiom Chuiko <v-achuiko@expedia.com>
2018-10-17 17:43:06 +0300

    update comments to populate ratings

--- interface/src/main/scala/com/artkostm/posters/interfaces/event/Models.scala
+++ interface/src/main/scala/com/artkostm/posters/interfaces/event/Models.scala
@@ -1,5 +1,5 @@
 package com.artkostm.posters.interfaces.event
 
-case class Comment(author: String, date: String, text: String)
+case class Comment(author: String, date: String, text: String, rating: Option[String] = None)
 
 case class EventInfo(description: String, photos: List[String], comments: List[Comment])
