9205b7bb5e4ef69c0e3fc33c85801286a165f319
Artsiom Chuiko <v-achuiko@expedia.com>
2018-10-18 14:50:08 +0300

    update test case

--- worker/src/main/scala/com/artkostm/posters/worker/scraper/ScraperModule.scala
+++ worker/src/main/scala/com/artkostm/posters/worker/scraper/ScraperModule.scala
@@ -4,19 +4,23 @@ import cats.effect.{Async, IO}
 import com.artkostm.posters.worker.config.{AppConfig, AppConfiguration}
 import fs2.StreamApp
 import fs2.StreamApp.ExitCode
+import org.joda.time.DateTime
 
 class ScraperModule[F[_]: Async](appConfig: AppConfig) {
-  val eventScraper = new EventInfoScraper[F](appConfig.scraper)
+  val eventScraper = new EventScraper[F](appConfig.scraper)
 }
 
 object testS extends StreamApp[IO] {
   override def stream (args: List[String], requestShutdown: IO[Unit] ): fs2.Stream[IO, StreamApp.ExitCode] = {
-    fs2.Stream.eval(AppConfiguration.load).flatMap (appConfig => {
-      println (appConfig)
-      fs2.Stream.eval(new EventInfoScraper[IO](appConfig.scraper).event("https://afisha.tut.by/film/venom/"))
-    }).flatMap(info => {
-      println(info)
-      fs2.Stream.eval(IO.pure (ExitCode.Success))
-    })
+    val stream = for {
+      config <- fs2.Stream.eval(AppConfiguration.load)
+      scraper <- fs2.Stream.eval(IO.pure(new EventScraper[IO](config.scraper)))
+      day <- fs2.Stream.range(1, 31).covary[IO]
+      event <- fs2.Stream.eval(scraper.event(DateTime.now().plusDays(day)))
+    } yield {
+      println(event)
+    }
+
+    fs2.Stream.eval(stream.compile.drain.attempt.map(_.fold(_ => 1, _ => 0)).map(ExitCode.fromInt(_)))
   }
 }
