6dd2451c9b4feed0cb4b4af8d276420ef864110a
Artsiom Chuiko <v-achuiko@expedia.com>
2018-10-19 14:39:14 +0300

    add example

--- worker/src/main/scala/com/artkostm/posters/worker/scraper/ScraperModule.scala
+++ worker/src/main/scala/com/artkostm/posters/worker/scraper/ScraperModule.scala
@@ -1,5 +1,8 @@
 package com.artkostm.posters.worker.scraper
 
+import bloomfilter.CanGenerateHashFrom
+import bloomfilter.CanGenerateHashFrom.CanGenerateHashFromString
+import bloomfilter.mutable.BloomFilter
 import cats.effect.{Async, IO}
 import com.artkostm.posters.worker.config.{AppConfig, AppConfiguration}
 import fs2.StreamApp
@@ -23,4 +26,35 @@ object testS extends StreamApp[IO] {
 
     fs2.Stream.eval(stream.compile.drain.attempt.map(_.fold(_ => 1, _ => 0)).map(ExitCode.fromInt(_)))
   }
+}
+
+object ttttttt extends App {
+  case class Request(date: DateTime, name: String)
+  val expectedElements = 6
+  val falsePositiveRate = 0.1
+
+  implicit val requestHashGenerator = new CanGenerateHashFrom[Request] {
+    override def generateHash(from: Request): Long =
+      CanGenerateHashFromString.generateHash(s"${from.date.toLocalDate}-${from.name}")
+  }
+
+  val bf = BloomFilter[Request](expectedElements, falsePositiveRate)
+
+  Runtime.getRuntime.addShutdownHook(new Thread(){
+    override def run(): Unit = {
+      println("CLOSING THE PROGRAM")
+      bf.dispose()
+    }
+  })
+  // Put an element
+  bf.add(Request(DateTime.now, "1"))
+  bf.add(Request(DateTime.now, "3"))
+  bf.add(Request(DateTime.now, "5"))
+  bf.add(Request(DateTime.now, "7"))
+  bf.add(Request(DateTime.now, "10"))
+
+  List(1, 4, 6, 12, 43).foreach(d => println(bf.mightContain(Request(DateTime.now, d.toString))))
+
+  bf.writeTo(System.out)
+  Thread.sleep(50000)
 }
