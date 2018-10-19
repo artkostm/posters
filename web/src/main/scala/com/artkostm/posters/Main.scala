3e6f1b9ffc070651075e37821349192d33765d0f
Artsiom Chuiko <v-achuiko@expedia.com>
2018-10-19 17:49:33 +0300

    small update

--- web/src/main/scala/com/artkostm/posters/Main.scala
+++ web/src/main/scala/com/artkostm/posters/Main.scala
@@ -1,8 +1,6 @@
 package com.artkostm.posters
 
-import java.net.URI
 import java.time.Instant
-import java.util.Date
 
 import eu.timepit.refined.api.Refined
 import eu.timepit.refined.string.MatchesRegex
@@ -13,7 +11,6 @@ import com.github.plokhotnyuk.jsoniter_scala.core._
 import fs2.StreamApp
 import org.http4s._
 import org.http4s.dsl.io._
-import org.joda.time.DateTime
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
@@ -23,31 +20,17 @@ object Main extends StreamApp[IO] {
   case class Personalities(info: String)
   case class User(name: String, personalities: List[Personalities], birth: Instant)
 
-  implicit val codec: JsonValueCodec[User] = JsonCodecMaker.make[User](CodecMakerConfig())
-
-  import ciris.env
-
-  import ciris.refined._
-
-  println(env[ApiKey]("API_KEY").value match {
-    case Right(key) => s"Here is the key: $key"
-    case _ => "Something went wrong"
-  })
-
-  println(env[URI]("pg").value match {
-    case Right(url) => s"Here is the url: $url"
-    case _ => "Something went wrong"
-  })
+  object User {
+    implicit val codec: JsonValueCodec[User] = JsonCodecMaker.make[User](CodecMakerConfig())
+  }
 
   val helloWorldService = HttpService[IO] {
     case GET -> Root / "hello" / name =>
-      Ok(new String(writeToArray(User(name, List(Personalities(name.toUpperCase)), Instant.now())), "UTF-8"))
+      Ok(User(name, List(Personalities(name.toUpperCase)), Instant.now()))
   }
 
   import fs2.Stream
-
   import fs2.StreamApp.ExitCode
-
   import org.http4s.server.blaze._
 
   override def stream(args: List[String], requestShutdown: IO[Unit]): Stream[IO, ExitCode] =
@@ -55,11 +38,4 @@ object Main extends StreamApp[IO] {
       .bindHttp(8080, "localhost")
       .mountService(helloWorldService, "/")
       .serve
-//  val builder = BlazeBuilder[IO].bindHttp(8080, "localhost").mountService(helloWorldService, "/").start
-//
-//  val server = builder.unsafeRunSync()
-//
-//  Thread.sleep(100000)
-//
-//  server.shutdown.unsafeRunSync()
 }//extends PostersServer
