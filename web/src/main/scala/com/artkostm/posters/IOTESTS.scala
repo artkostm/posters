75cea37f23d1b80b261ca70f9270552eaeb4a1f7
Artsiom Chuiko <Artsiom_Chuiko@epam.com>
2018-12-07 17:29:20 +0300

    update tests

--- web/src/main/scala/com/artkostm/posters/IOTESTS.scala
+++ web/src/main/scala/com/artkostm/posters/IOTESTS.scala
@@ -6,10 +6,10 @@ import cats.effect.IO
 import scala.concurrent._
 
 object IOTESTS extends App {
-  val Main = ExecutionContext.global
+  val Main           = ExecutionContext.global
   val BlockingFileIO = ExecutionContext.fromExecutor(Executors.newCachedThreadPool())
   val test = for {
-    _ <- IO(println("first: " + Thread.currentThread().getName))
+    _   <- IO(println("first: " + Thread.currentThread().getName))
     lol <- IO.shift(BlockingFileIO).flatMap(_ => IO.shift(Main)).flatMap(_ => as)
     _ <- {
       IO(println("second " + lol + s": ${Thread.currentThread().getName}"))
@@ -18,11 +18,11 @@ object IOTESTS extends App {
 
   test.unsafeRunAsync {
     case Right(a) => println(a)
-    case Left(e) => e.printStackTrace()
+    case Left(e)  => e.printStackTrace()
   }
 
   Thread.sleep(3000)
-  def as: IO[String] = IO.async[String]{ cb =>
+  def as: IO[String] = IO.async[String] { cb =>
     println("async: " + Thread.currentThread().getName)
     cb(Right("lol"))
 //    cb(Left(new RuntimeException))
