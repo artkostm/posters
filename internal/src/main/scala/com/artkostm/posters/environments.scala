0e95d836e0b0d07959109305e165f8c21fe74c98
Artsiom Chuiko <Artsiom_Chuiko@epam.com>
2018-12-07 17:26:08 +0300

    update environments

--- internal/src/main/scala/com/artkostm/posters/environments.scala
+++ internal/src/main/scala/com/artkostm/posters/environments.scala
@@ -5,8 +5,8 @@ object environments {
 
   sealed abstract class AppEnvironment extends EnumEntry
   object AppEnvironment extends Enum[AppEnvironment] {
-    case object Local extends AppEnvironment
-    case object Heroku extends AppEnvironment
+    case object Local      extends AppEnvironment
+    case object Heroku     extends AppEnvironment
     case object Production extends AppEnvironment
 
     val values = findValues
