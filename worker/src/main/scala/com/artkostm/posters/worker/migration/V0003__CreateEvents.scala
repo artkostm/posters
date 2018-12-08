1a33485c8409c1e14202dd79a03bfa37b5732fc9
Artsiom <artkostm@gmail.com>
2018-12-08 17:40:16 +0300

    add create events migration

--- worker/src/main/scala/com/artkostm/posters/worker/migration/V0003__CreateEvents.scala
+++ worker/src/main/scala/com/artkostm/posters/worker/migration/V0003__CreateEvents.scala
@@ -1,5 +1,18 @@
 package com.artkostm.posters.worker.migration
 
-class V0003__CreateEvents {
+import doobie._
+import doobie.implicits._
+import com.artkostm.posters.worker.DoobieMigration
 
+class V0003__CreateEvents extends DoobieMigration {
+  override def migrate: ConnectionIO[_] =
+    sql"""
+         CREATE TABLE IF NOT EXISTS events
+         (
+         	date timestamp NOT NULL
+         		CONSTRAINT pk_events
+         			PRIMARY KEY,
+         	categories jsonb NOT NULL
+         )
+       """.update.run
 }
