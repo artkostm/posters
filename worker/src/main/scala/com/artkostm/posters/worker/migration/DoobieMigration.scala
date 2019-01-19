49d10c42544fca339e07f693fff4566def4214f2
Artsiom <artkostm@gmail.com>
2019-01-19 21:40:49 +0300

    update db migration implementation

--- worker/src/main/scala/com/artkostm/posters/worker/migration/DoobieMigration.scala
+++ worker/src/main/scala/com/artkostm/posters/worker/migration/DoobieMigration.scala
@@ -1,9 +1,10 @@
-package com.artkostm.posters.worker
+package com.artkostm.posters.worker.migration
 
 import java.sql.Connection
 
 import cats.effect.{IO, Sync}
 import com.artkostm.posters.worker.config.AppConfig
+import com.artkostm.posters.worker.migration.v1.V0001__CreateVisitors
 import doobie._
 import doobie.implicits._
 import org.flywaydb.core.Flyway
@@ -22,7 +23,7 @@ object DoobieMigration {
   def run[F[_]](config: AppConfig)(implicit F: Sync[F]): F[Int] = F.delay {
     println(config)
     val flyway   = new Flyway
-    val location = classOf[migration.V0001__CreateVisitors].getPackage.getName.replace(".", "/")
+    val location = classOf[V0001__CreateVisitors].getPackage.getName.replace(".", "/")
 
 //    flyway.setDataSource("jdbc:h2:~/test;MODE=PostgreSQL", "", "")
     flyway.setDataSource(config.db.url.value, config.db.user, config.db.password)
