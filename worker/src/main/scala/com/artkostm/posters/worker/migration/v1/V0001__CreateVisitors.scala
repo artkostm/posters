4f0a64b0d655a2f0bf71b323facca2a374d51abf
Artsiom <artkostm@gmail.com>
2019-01-19 21:39:12 +0300

    small refactoring

--- worker/src/main/scala/com/artkostm/posters/worker/migration/v1/V0001__CreateVisitors.scala
+++ worker/src/main/scala/com/artkostm/posters/worker/migration/v1/V0001__CreateVisitors.scala
@@ -2,7 +2,6 @@ package com.artkostm.posters.worker.migration
 
 import doobie._
 import doobie.implicits._
-import com.artkostm.posters.worker.DoobieMigration
 
 final class V0001__CreateVisitors extends DoobieMigration {
   override def migrate: ConnectionIO[_] =
--- worker/src/main/scala/com/artkostm/posters/worker/migration/v1/V0003__CreateEvents.scala
+++ worker/src/main/scala/com/artkostm/posters/worker/migration/v1/V0003__CreateEvents.scala
@@ -2,7 +2,6 @@ package com.artkostm.posters.worker.migration
 
 import doobie._
 import doobie.implicits._
-import com.artkostm.posters.worker.DoobieMigration
 
 class V0003__CreateEvents extends DoobieMigration {
   override def migrate: ConnectionIO[_] =
