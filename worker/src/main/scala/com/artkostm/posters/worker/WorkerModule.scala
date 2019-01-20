ed2651abee468111585a0ef41c96230c539513a1
Artsiom <artkostm@gmail.com>
2019-01-20 19:41:49 +0300

    update worker module

--- worker/src/main/scala/com/artkostm/posters/worker/WorkerModule.scala
+++ worker/src/main/scala/com/artkostm/posters/worker/WorkerModule.scala
@@ -1,5 +1,6 @@
 package com.artkostm.posters.worker
 
+import cats.implicits._
 import cats.effect.Effect
 import com.artkostm.posters.worker.config.{AppConfig, AppConfiguration}
 import com.artkostm.posters.worker.migration.DoobieMigration
@@ -8,10 +9,10 @@ import doobie.hikari.HikariTransactor
 class WorkerModule[F[_]: Effect](config: AppConfig, val xa: HikariTransactor[F]) {}
 
 object WorkerModule {
-  def init[F[_]: Effect]: F[WorkerModule[F]] =
+  def init[F[_]: Effect](): F[WorkerModule[F]] =
     for {
-      config: AppConfig <- AppConfiguration.load[F]
-      _                 <- DoobieMigration.run[F](config)
-      xa                <- DoobieMigration.transactor(config.db)
+      config <- AppConfiguration.load[F]
+      _      <- DoobieMigration.run[F](config)
+      xa     <- DoobieMigration.transactor(config.db)
     } yield new WorkerModule[F](config, xa)
 }
