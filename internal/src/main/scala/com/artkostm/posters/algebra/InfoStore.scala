2416b7a698c0d918a494a28273f1297ee62580cd
Artsiom <artkostm@gmail.com>
2019-02-02 21:26:27 +0300

    info store

--- internal/src/main/scala/com/artkostm/posters/algebra/InfoStore.scala
+++ internal/src/main/scala/com/artkostm/posters/algebra/InfoStore.scala
@@ -1,3 +1,7 @@
 package com.artkostm.posters.algebra
+import com.artkostm.posters.interfaces.event.EventInfo
 
-trait InfoStore[F[_]] {}
+trait InfoStore[F[_]] {
+  def save(info: EventInfo): F[EventInfo]
+  def find(link: String): F[Option[EventInfo]]
+}
