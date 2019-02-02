fd51aa95e93f61b055604e6874788ebdb3865f60
Artsiom <artkostm@gmail.com>
2019-02-02 21:26:49 +0300

    doobie-jsoniter integration

--- internal/src/main/scala/com/artkostm/posters/doobie/package.scala
+++ internal/src/main/scala/com/artkostm/posters/doobie/package.scala
@@ -1,5 +1,21 @@
 package com.artkostm.posters
 
-package object doobie {
+import java.nio.charset.StandardCharsets
 
+import org.postgresql.util.PGobject
+import com.github.plokhotnyuk.jsoniter_scala.core._
+
+package object doobie {
+  implicit def jsonbMeta[A: Manifest: JsonValueCodec]: doobie.Meta[A] =
+    doobie.Meta
+      .other[PGobject]("jsonb")
+      .xmap[A](
+      pgObject => readFromArray(pgObject.getValue.getBytes(StandardCharsets.UTF_8)),
+      jsonObject => {
+        val pgObject = new PGobject()
+        pgObject.setType("jsonb")
+        pgObject.setValue(new String(writeToArray(jsonObject), StandardCharsets.UTF_8))
+        pgObject
+      }
+    )
 }
