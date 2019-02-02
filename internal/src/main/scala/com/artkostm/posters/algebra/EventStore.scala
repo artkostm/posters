6367bf1ba071890b07a3f55850e82b213a796596
Artsiom <artkostm@gmail.com>
2019-02-02 23:58:34 +0300

    add event store interpreter

--- internal/src/main/scala/com/artkostm/posters/algebra/EventStore.scala
+++ internal/src/main/scala/com/artkostm/posters/algebra/EventStore.scala
@@ -1,3 +1,10 @@
 package com.artkostm.posters.algebra
+import java.time.Instant
 
-trait EventStore[F[_]] {}
+import com.artkostm.posters.interfaces.schedule.{Category, Day}
+
+trait EventStore[F[_]] {
+  def save(day: Day): F[Day]
+  def findByDate(day: Instant): F[Option[Day]]
+  def findByName(names: List[String]): F[List[Category]]
+}
