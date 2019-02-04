036ecbca947b46679ac3e779c5cb1c16bfc55e0b
Artsiom <artkostm@gmail.com>
2019-02-04 19:57:46 +0300

    small update

--- internal/src/main/scala/com/artkostm/posters/algebra/EventStore.scala
+++ internal/src/main/scala/com/artkostm/posters/algebra/EventStore.scala
@@ -4,7 +4,7 @@ import java.time.Instant
 import com.artkostm.posters.interfaces.schedule.{Category, Day}
 
 trait EventStore[F[_]] {
-  def delete(today: Instant): F[Int]
+  def deleteOld(today: Instant): F[Int]
   def save(day: Day): F[Day]
   def findByDate(day: Instant): F[Option[Day]]
   def findByName(names: List[String]): F[List[Category]]
