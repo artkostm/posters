0495d2778165c63b5bfc43eb2555e3ef253daf4d
Artsiom Chuiko <artkostm@gmail.com>
2019-02-04 17:17:05 +0300

    update models

--- interface/src/main/scala/com/artkostm/posters/interfaces/intent/Models.scala
+++ interface/src/main/scala/com/artkostm/posters/interfaces/intent/Models.scala
@@ -3,6 +3,6 @@ package com.artkostm.posters.interfaces.intent
 import java.time.Instant
 
 final case class Intent(date: Instant,
-                        eventName: String,
+                        event_name: String,
                         vids: List[String] = List.empty,
                         uids: List[String] = List.empty)
