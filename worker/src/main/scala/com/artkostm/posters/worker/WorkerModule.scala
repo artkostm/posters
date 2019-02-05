3f11a2aa82839112192a981b8cbe180388b36be8
Artsiom <artkostm@gmail.com>
2019-02-05 23:10:09 +0300

    update worker module

--- worker/src/main/scala/com/artkostm/posters/worker/WorkerModule.scala
+++ worker/src/main/scala/com/artkostm/posters/worker/WorkerModule.scala
@@ -1,15 +1,18 @@
 package com.artkostm.posters.worker
 
 import cats.implicits._
-import cats.effect.{ContextShift, Effect, Sync}
-import com.artkostm.posters.interpreter.{EventStoreInterpreter, InfoStoreInterpreter}
+import cats.effect.{ContextShift, Effect, Resource, Sync}
+import com.artkostm.posters.interpreter.{EventStoreInterpreter, InfoStoreInterpreter, VisitorStoreInterpreter}
 import com.artkostm.posters.worker.config.{AppConfig, AppConfiguration}
 import com.artkostm.posters.worker.migration.DoobieMigration
+import com.artkostm.posters.worker.scraper.EventScraper
 import doobie.hikari.HikariTransactor
 
 class WorkerModule[F[_]: Effect](config: AppConfig, val xa: HikariTransactor[F]) {
-  private lazy val infoStore  = new InfoStoreInterpreter()
-  private lazy val eventStore = new EventStoreInterpreter()
+  lazy val scraper      = new EventScraper[F](config.scraper)
+  private lazy val infoStore    = new InfoStoreInterpreter()
+  private lazy val eventStore   = new EventStoreInterpreter()
+  private lazy val visitorStore = new VisitorStoreInterpreter()
 }
 
 object WorkerModule {
@@ -17,6 +20,6 @@ object WorkerModule {
     for {
       config <- AppConfiguration.load[F]
       _      <- DoobieMigration.run[F](config)
-      xa     <- DoobieMigration.transactor(config.db).use(xa => F.delay(new WorkerModule[F](config, xa)))
-    } yield xa
+      module <- DoobieMigration.transactor(config.db).use(xa => F.delay(new WorkerModule[F](config, xa)))
+    } yield module
 }
