03a5492debf48ef877a5efd71b5732bc3db953dc
Artsiom <artkostm@gmail.com>
2019-02-08 23:19:29 +0300

    update collector

--- worker/src/main/scala/com/artkostm/posters/worker/collector/EventCollector.scala
+++ worker/src/main/scala/com/artkostm/posters/worker/collector/EventCollector.scala
@@ -3,24 +3,23 @@ package com.artkostm.posters.worker.collector
 import java.time.Instant
 import java.time.temporal.ChronoUnit
 
-import cats.implicits._
-import cats.effect.{Concurrent, Effect, Timer}
-import com.artkostm.posters.algebra.{EventStore, InfoStore}
+import cats.effect.{Concurrent, Timer}
+import com.artkostm.posters.algebra.{EventStore, InfoStore, VisitorStore}
 import com.artkostm.posters.interfaces.event.EventInfo
 import com.artkostm.posters.worker.scraper.EventScraper
 import fs2.Stream
 
 import scala.concurrent.duration._
-import doobie.implicits._
 
-class EventCollector[F[_]: Timer: Concurrent](scraper: EventScraper[F],
-                                              eventStore: EventStore[F],
-                                              infoStore: InfoStore[F])(implicit F: Effect[F]) {
+class EventCollector[F[_]: Timer](scraper: EventScraper[F],
+                                  eventStore: EventStore[F],
+                                  infoStore: InfoStore[F],
+                                  visitorStore: VisitorStore[F])(implicit F: Concurrent[F]) {
   def collect() = {
     val dayStream = Stream
       .range(-2, 31)
       .covary[F]
-      .map(Instant.now().truncatedTo(ChronoUnit.DAYS).plus(_, ChronoUnit.DAYS))
+      .map(Instant.now().plus(_, ChronoUnit.DAYS).truncatedTo(ChronoUnit.DAYS))
       .mapAsyncUnordered(4)(scraper.event)
 
     val insertDays = dayStream.mapAsyncUnordered(4)(eventStore.save)
@@ -32,7 +31,10 @@ class EventCollector[F[_]: Timer: Concurrent](scraper: EventScraper[F],
         case None       => F.raiseError[EventInfo](new RuntimeException("cannot save empty event info"))
       }
 
-    val graph = Stream(insertDays, saveEvents).parJoin(2)
+    val graph = Stream.eval(eventStore.deleteOld(Instant.now())) >>
+      Stream.eval(infoStore.deleteOld()) >>
+      Stream.eval(visitorStore.deleteOld(Instant.now())) >>
+      Stream(insertDays, saveEvents).parJoin(2)
     graph.merge(Stream.awakeEvery[F](24 hours) >> graph)
   }
 }
