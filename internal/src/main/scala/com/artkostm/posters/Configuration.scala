da9f7946b083edf112d382c8dbae7b5638feed2b
Artsiom Chuiko <artkostm@gmail.com>
2019-02-13 18:17:44 +0300

    update db configuration

--- internal/src/main/scala/com/artkostm/posters/Configuration.scala
+++ internal/src/main/scala/com/artkostm/posters/Configuration.scala
@@ -1,7 +1,10 @@
 package com.artkostm.posters
 
+import cats.effect._
 import cats.MonadError
 import ciris._
+import doobie.hikari.HikariTransactor
+import doobie.util.ExecutionContexts
 import eu.timepit.refined.types.numeric.PosInt
 import eu.timepit.refined.types.string.NonEmptyString
 import eu.timepit.refined.auto._
@@ -22,28 +25,36 @@ object Configuration {
     DatabaseConfig(_,
                    driver = "org.postgresql.Driver",
                    user = "test",
-                   password = "12345",
-                   numThreads = 10,
-                   maxConnections = 15,
-                   minConnections = 4)
+                   password = "12345")
 
   val buildDbConfigForHeroku: (String, NonEmptyString, NonEmptyString) => DatabaseConfig =
     (url, user, password) =>
       DatabaseConfig(url,
                      driver = "org.postgresql.Driver",
                      user = user,
-                     password = password,
-                     numThreads = 10,
-                     maxConnections = 15,
-                     minConnections = 4)
+                     password = password)
 
   final case class DatabaseConfig(
       url: String,
       driver: NonEmptyString,
       user: String = "",
       password: String = "",
-      numThreads: PosInt,
-      maxConnections: PosInt,
-      minConnections: PosInt
+      numThreads: PosInt = 32,
+      maxConnections: PosInt = 15,
+      minConnections: PosInt = 14
   )
+
+  object DatabaseConfig {
+    def transactor[F[_]: Async: ContextShift](dbConfig: DatabaseConfig): Resource[F, HikariTransactor[F]] =
+      for {
+        ce <- ExecutionContexts.fixedThreadPool[F](dbConfig.numThreads)
+        te <- ExecutionContexts.cachedThreadPool[F]
+        xa <- HikariTransactor.newHikariTransactor[F](driverClassName = dbConfig.driver.value,
+                                                      url = dbConfig.url,
+                                                      user = dbConfig.user,
+                                                      pass = dbConfig.password,
+                                                      connectEC = ce,
+                                                      transactEC = te)
+      } yield xa
+  }
 }
