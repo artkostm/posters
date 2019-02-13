ba67779082b3bba4c9ba7d301d2f8cecda5642de
Artsiom <artkostm@gmail.com>
2019-02-13 22:57:37 +0300

    update info endpoint

--- web/src/main/scala/com/artkostm/posters/endpoint/InfoEndpoint.scala
+++ web/src/main/scala/com/artkostm/posters/endpoint/InfoEndpoint.scala
@@ -1,7 +1,9 @@
 package com.artkostm.posters.endpoint
 
+import cats.data.EitherT
 import cats.implicits._
 import cats.effect.Effect
+import com.artkostm.posters.EventInfoNotFound
 import com.artkostm.posters.algebra.InfoStore
 import com.artkostm.posters.interfaces.event.EventInfo
 import com.github.plokhotnyuk.jsoniter_scala.core.JsonValueCodec
@@ -15,13 +17,15 @@ class InfoEndpoint[F[_]: Effect](repository: InfoStore[F]) extends Http4sDsl[F] 
   /* Parses out link query param */
   object LinkMatcher extends QueryParamDecoderMatcher[String]("link")
 
-  implicit val codec: JsonValueCodec[EventInfo] = JsonCodecMaker.make[EventInfo](CodecMakerConfig())
+  implicit val eventInfoCodec: JsonValueCodec[EventInfo] = JsonCodecMaker.make[EventInfo](CodecMakerConfig())
+
+  implicit val eventInfoNotFoundCodec: JsonValueCodec[EventInfoNotFound] = JsonCodecMaker.make[EventInfoNotFound](CodecMakerConfig())
 
   private def getEventInfo(): HttpRoutes[F] = HttpRoutes.of[F] {
     case GET -> Root / "events" :? LinkMatcher(link) =>
       for {
-        info <- repository.find(link)
-        resp <- info.map(Ok(_)).getOrElse(NotFound("The info was not found!"))
+        info <- EitherT.fromOptionF(repository.find(link), EventInfoNotFound(s"Cannot find event using $link")).value
+        resp <- info.fold(NotFound(_), Ok(_))
       } yield resp
   }
 
@@ -31,3 +35,4 @@ class InfoEndpoint[F[_]: Effect](repository: InfoStore[F]) extends Http4sDsl[F] 
 object InfoEndpoint {
   def apply[F[_]: Effect](repository: InfoStore[F]): HttpRoutes[F] = new InfoEndpoint(repository).endpoints()
 }
+
