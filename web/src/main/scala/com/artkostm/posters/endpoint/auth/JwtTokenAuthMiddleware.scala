176fdc3e7699242b2be96af067d483ed9654c5af
Artsiom <artkostm@gmail.com>
2019-02-16 15:34:20 +0300

    update jwt middleware to use User model

--- web/src/main/scala/com/artkostm/posters/endpoint/auth/JwtTokenAuthMiddleware.scala
+++ web/src/main/scala/com/artkostm/posters/endpoint/auth/JwtTokenAuthMiddleware.scala
@@ -4,7 +4,9 @@ import cats.data.{EitherT, Kleisli, OptionT}
 import cats.effect.Sync
 import cats.syntax.applicativeError._
 import cats.syntax.functor._
+import com.artkostm.posters.config.ApiConfig
 import com.artkostm.posters.endpoint.auth.JwtTokenAuthMiddleware.AuthConfig
+import com.artkostm.posters.interfaces.auth.User
 import org.http4s.Credentials.Token
 import org.http4s.{AuthScheme, AuthedService, Request}
 import org.http4s.dsl.Http4sDsl
@@ -12,34 +14,35 @@ import org.http4s.headers.Authorization
 import org.http4s.server.AuthMiddleware
 import tsec.jws.mac.JWTMac
 import tsec.mac.jca.{HMACSHA256, MacSigningKey, MacVerificationError}
+import eu.timepit.refined.auto._
 
 object JwtTokenAuthMiddleware {
-  def apply[F[_] : Sync](apiToken: Option[String]): F[AuthMiddleware[F, String]] =
-    new Middleware[F](apiToken).middleware
+  def apply[F[_]: Sync](api: ApiConfig): F[AuthMiddleware[F, User]] =
+    new Middleware[F](api).middleware
 
   case class AuthConfig(jwtKey: MacSigningKey[HMACSHA256])
 }
 
-class Middleware[F[_]](apiToken: Option[String])(implicit F: Sync[F]) {
+class Middleware[F[_]](api: ApiConfig)(implicit F: Sync[F]) {
 
-  private val ifEmpty = F.raiseError[AuthMiddleware[F, String]](new Exception("Api Token not found"))
+  private val ifEmpty = F.raiseError[AuthMiddleware[F, User]](new Exception("Api Token not found"))
 
-  private def generateJwtKey(token: String): F[MacSigningKey[HMACSHA256]] = {
+  private def generateJwtKey(token: String): F[MacSigningKey[HMACSHA256]] =
     F.catchNonFatal(HMACSHA256.unsafeBuildKey(token.getBytes))
-  }
 
-  val middleware: F[AuthMiddleware[F, String]] = apiToken.fold(ifEmpty) { token =>
+  val middleware: F[AuthMiddleware[F, User]] = Option(api.token).fold(ifEmpty) { token =>
     generateJwtKey(token).map { jwtKey =>
       val config = AuthConfig(jwtKey)
-      new JwtTokenAuthMiddleware[F](config).middleware
+      new JwtTokenAuthMiddleware[F](config, api.key.value).middleware
     }
   }
 }
 
-class JwtTokenAuthMiddleware[F[_] : Sync](config: AuthConfig) extends Http4sDsl[F] {
+class JwtTokenAuthMiddleware[F[_]: Sync](config: AuthConfig, apiKey: String) extends Http4sDsl[F] {
 
+  // TODO: add error
   private val onFailure: AuthedService[String, F] =
-    Kleisli(req => OptionT.liftF(Forbidden(req.authInfo)))
+    Kleisli(_ => OptionT.liftF(Forbidden("Forbidden!")))
 
   private def bearerTokenFromRequest(request: Request[F]): OptionT[F, String] =
     OptionT.fromOption[F] {
@@ -48,23 +51,28 @@ class JwtTokenAuthMiddleware[F[_] : Sync](config: AuthConfig) extends Http4sDsl[
       }
     }
 
-  private def verifyToken(request: Request[F],
-                          jwtKey: MacSigningKey[HMACSHA256]): OptionT[F, String] =
+  private def verifyToken(request: Request[F], jwtKey: MacSigningKey[HMACSHA256]): OptionT[F, User] = {
+    import cats.implicits._
     for {
-      token       <- bearerTokenFromRequest(request)
-      verified    <- OptionT.liftF(JWTMac.verifyAndParse[F, HMACSHA256](token, jwtKey))
-      accessToken <- OptionT.fromOption[F](verified.body.subject)
+      token    <- bearerTokenFromRequest(request)
+      verified <- OptionT.liftF(JWTMac.verifyAndParse[F, HMACSHA256](token, jwtKey))
+      accessToken <- OptionT.fromOption[F](
+                      (verified.body.subject, verified.body.subject).bisequence.map(User.tupled)
+                    )
     } yield accessToken
+  }
 
-  private def authUser(jwtKey: MacSigningKey[HMACSHA256]): Kleisli[F, Request[F], Either[String, String]] =
+  private def authUser(jwtKey: MacSigningKey[HMACSHA256]): Kleisli[F, Request[F], Either[String, User]] =
     Kleisli { request =>
-      verifyToken(request, jwtKey).value.map { option =>
-        Either.cond[String, String](option.isDefined, option.get, "Unable to authorize token")
-      }.recoverWith {
-        case MacVerificationError(msg) => EitherT.leftT(msg).value
-      }
+      verifyToken(request, jwtKey).value
+        .map { option =>
+          Either.cond[String, User](option.map(_.apiKey).contains(apiKey), option.get, "Unable to authorize token")
+        }
+        .recoverWith {
+          case MacVerificationError(msg) => EitherT.leftT(msg).value
+        }
     }
 
-  def middleware: AuthMiddleware[F, String] =
+  def middleware: AuthMiddleware[F, User] =
     AuthMiddleware(authUser(config.jwtKey), onFailure)
 }
