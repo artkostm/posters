d01118991086d726c4b47183ba7f0b993b669dd8
Artsiom <artkostm@gmail.com>
2019-02-17 15:01:20 +0300

    add a few methods

--- internal/src/main/scala/com/artkostm/posters/algebra/EventStore.scala
+++ internal/src/main/scala/com/artkostm/posters/algebra/EventStore.scala
@@ -8,5 +8,6 @@ trait EventStore[F[_]] {
   def deleteOld(today: Instant): F[Int]
   def save(day: Day): F[Day]
   def findByDate(day: Instant): F[Option[Day]]
-  def findByName(names: NonEmptyList[String]): F[List[Category]]
+  def findByNames(names: NonEmptyList[String]): F[List[Category]]
+  def findByNameAndDate(name: String, date: Instant): F[Option[Category]]
 }
