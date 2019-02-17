bb40a45ac245ade89b8b480b79dfcc84d257fc3f
Artsiom <artkostm@gmail.com>
2019-02-17 15:02:24 +0300

    update main

--- web/src/main/scala/com/artkostm/posters/Main.scala
+++ web/src/main/scala/com/artkostm/posters/Main.scala
@@ -1,63 +1,25 @@
 package com.artkostm.posters
 
-import java.time.Instant
+import java.time.format.DateTimeFormatterBuilder
+import java.time.{Instant, LocalDate, ZoneId}
+import java.time.temporal.{ChronoField, ChronoUnit, TemporalAccessor}
 
-import eu.timepit.refined.api.Refined
-import eu.timepit.refined.string.MatchesRegex
-import eu.timepit.refined.W
 import cats.effect._
-import com.artkostm.posters.Configuration.DatabaseConfig
-import com.artkostm.posters.config.WebConfiguration
-import com.artkostm.posters.endpoint.InfoEndpoint
 import com.artkostm.posters.endpoint.auth.JwtTokenAuthMiddleware
 import com.artkostm.posters.interfaces.dialog.v2._
-import com.artkostm.posters.interpreter.InfoStoreInterpreter
-import com.github.plokhotnyuk.jsoniter_scala.macros._
-import com.github.plokhotnyuk.jsoniter_scala.core._
-import doobie.ExecutionContexts
-import doobie.hikari.HikariTransactor
-import org.http4s.implicits._
-import org.http4s._
-import org.http4s.dsl.io._
 import org.http4s.server.Router
 
-import scala.concurrent.Future
-
 object Main extends IOApp {
-//  type ApiKey = String Refined MatchesRegex[W.`"[a-zA-Z0-9]{25,40}"`.T]
-//
-//  case class Personalities(info: String)
-//  case class User(name: String, personalities: List[Personalities], birth: Instant)
-//
-//  object User {
-//    implicit val codec: JsonValueCodec[User] = JsonCodecMaker.make[User](CodecMakerConfig())
-//  }
-//
-//  import com.artkostm.posters.jsoniter._
-//
-//  val helloWorldService = HttpService[IO] {
-//    case req @ POST -> Root / "hello" =>
-//      for {
-//        user <- req.as[User]
-//        resp <- Ok(User(user.name.toUpperCase, personalities = List.empty, Instant.now().minusSeconds(5000)))
-//      } yield resp
-//    case GET -> Root / "hello" / name =>
-//      Ok(User(name, List(Personalities(name.toUpperCase)), Instant.now()))
-//    case GET -> Root / "test1" =>
-//      Ok("hello")
-//  }
-
   import org.http4s.server.blaze._
+  import org.http4s.implicits._
   import cats.implicits._
   override def run(args: List[String]): IO[ExitCode] =
     (for {
-      config    <- Resource.liftF(WebConfiguration.load[IO])
-      xa        <- DatabaseConfig.transactor[IO](config.db)
-      infoStore = new InfoStoreInterpreter(xa.trans)
-      auth      <- Resource.liftF(JwtTokenAuthMiddleware[IO](config.api))
+      module <- WebModule.init
+      auth   <- Resource.liftF(JwtTokenAuthMiddleware[IO](module.config.api))
       exit <- BlazeServerBuilder[IO]
                .bindHttp(8080, "localhost")
-               .withHttpApp(Router("/" -> auth(InfoEndpoint(infoStore))).orNotFound)
+               .withHttpApp(Router("/" -> auth(module.endpoints)).orNotFound)
                .resource
     } yield exit)
       .use(_ => IO.never)
@@ -85,7 +47,19 @@ object testMonocle extends App {
     case _          => println("empty")
   }
 
+  println(Instant.now().truncatedTo(ChronoUnit.DAYS))
+
 //  val pr = monocle.Prism.partial[DialogflowRequest, List[String]] {
 //    case DialogflowRequest(_, QueryResult(_, Parameters(category, _), _, _, _, _, _), _, _) => category
 //  }
+}
+
+
+object dateTest extends App {
+  val FMT = new DateTimeFormatterBuilder()
+    .appendPattern("yyyy/MM/dd")
+    .parseDefaulting(ChronoField.NANO_OF_DAY, 0)
+    .toFormatter()
+    .withZone(ZoneId.of("Europe/Minsk"))
+  println(FMT.parse("2019/02/17", (tmp: TemporalAccessor) => Instant.from(tmp)))
 }
