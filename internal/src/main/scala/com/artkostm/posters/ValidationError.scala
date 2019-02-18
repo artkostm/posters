5875935ebe9065120dbf1b3c2a1a52bfdfca3881
Artsiom <artkostm@gmail.com>
2019-02-18 20:28:55 +0300

    update validation errors

--- internal/src/main/scala/com/artkostm/posters/ValidationError.scala
+++ internal/src/main/scala/com/artkostm/posters/ValidationError.scala
@@ -1,9 +1,21 @@
 package com.artkostm.posters
 
+import com.github.plokhotnyuk.jsoniter_scala.core.JsonValueCodec
+import com.github.plokhotnyuk.jsoniter_scala.macros.{CodecMakerConfig, JsonCodecMaker}
+
 sealed trait ValidationError {
   def code: Int
   def msg: String
 }
 
-final case class EventInfoNotFound(msg: String, code: Int = 404) extends ValidationError
-final case class CategoryNotFound(msg: String, code: Int = 404) extends ValidationError
+object ValidationError {
+  final case class EventInfoNotFound(msg: String, code: Int = 404) extends ValidationError
+  final case class CategoryNotFound(msg: String, code: Int = 404) extends ValidationError
+
+
+  implicit val eventInfoNotFoundCodec: JsonValueCodec[EventInfoNotFound] =
+    JsonCodecMaker.make[EventInfoNotFound](CodecMakerConfig())
+
+  implicit val categoryNotFoundCodec: JsonValueCodec[CategoryNotFound] =
+    JsonCodecMaker.make[CategoryNotFound](CodecMakerConfig())
+}
