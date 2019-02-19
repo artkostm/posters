3fb066d28049e53ae25bb380f2ea00768518b4a3
Artsiom Chuiko <artkostm@gmail.com>
2019-02-19 15:04:27 +0300

    update category endpoint

--- web/src/main/scala/com/artkostm/posters/endpoint/CategoryEndpoint.scala
+++ web/src/main/scala/com/artkostm/posters/endpoint/CategoryEndpoint.scala
@@ -1,43 +1,21 @@
 package com.artkostm.posters.endpoint
 
-import java.time.{Instant, ZoneId}
-import java.time.format.DateTimeFormatterBuilder
-import java.time.temporal.{ChronoField, ChronoUnit, TemporalAccessor}
-
 import cats.data.EitherT
 import cats.implicits._
 import cats.effect.Effect
 import com.artkostm.posters.algebra.EventStore
 import com.artkostm.posters.categories.CategoryVar
 import com.artkostm.posters.interfaces.auth.User
-import com.artkostm.posters.interfaces.schedule.Category
-import com.github.plokhotnyuk.jsoniter_scala.core.JsonValueCodec
-import com.github.plokhotnyuk.jsoniter_scala.macros.{CodecMakerConfig, JsonCodecMaker}
-import org.http4s.{AuthedService, QueryParamDecoder}
+import com.artkostm.posters.scraper.Scraper
+import org.http4s.AuthedService
 import org.http4s.dsl.Http4sDsl
 
-class CategoryEndpoint[F[_]: Effect](repository: EventStore[F]) extends Http4sDsl[F] with EndpointsAware[F] {
+class CategoryEndpoint[F[_]: Effect](repository: EventStore[F], scraper: Scraper[F]) extends Http4sDsl[F] with EndpointsAware[F] {
   import com.artkostm.posters.jsoniter._
+  import ValueCodecs._
   import com.artkostm.posters.ValidationError._
 
-  // TODO: move into package at root
-  val FMT = new DateTimeFormatterBuilder()
-    .appendPattern("yyyy/MM/dd")
-    .parseDefaulting(ChronoField.NANO_OF_DAY, 0)
-    .toFormatter()
-    .withZone(ZoneId.of("Europe/Minsk"))
-
-  implicit val yearQueryParamDecoder: QueryParamDecoder[Instant] =
-    QueryParamDecoder[String].map(
-      FMT.parse(_, (temporal: TemporalAccessor) => Instant.from(temporal).truncatedTo(ChronoUnit.DAYS)))
-
-  object DateMatcher extends QueryParamDecoderMatcher[Instant]("date")
-
-  // TODO: move codecs to appropriate places
-  implicit val categoryCodec: JsonValueCodec[Category] = JsonCodecMaker.make[Category](CodecMakerConfig())
-
   private def getCategoryByName(): AuthedService[User, F] = AuthedService {
-    // TODO: add validation for the name (create enum and corresponding object with unapply)
     case GET -> Root / "categories" / CategoryVar(categoryName) :? DateMatcher(date) as _ =>
       for {
         category <- EitherT
@@ -52,6 +30,6 @@ class CategoryEndpoint[F[_]: Effect](repository: EventStore[F]) extends Http4sDs
 }
 
 object CategoryEndpoint {
-  def apply[F[_]: Effect](repository: EventStore[F]): AuthedService[User, F] =
-    new CategoryEndpoint(repository).endpoints
+  def apply[F[_]: Effect](repository: EventStore[F], scraper: Scraper[F]): AuthedService[User, F] =
+    new CategoryEndpoint(repository, scraper).endpoints
 }
