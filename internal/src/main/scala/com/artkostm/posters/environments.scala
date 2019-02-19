f3716ea717bb2142f326bfb57e546fba53b04a2b
Artsiom Chuiko <artkostm@gmail.com>
2019-02-19 11:42:59 +0300

    add categories enum

--- internal/src/main/scala/com/artkostm/posters/environments.scala
+++ internal/src/main/scala/com/artkostm/posters/environments.scala
@@ -12,3 +12,20 @@ object environments {
     val values = findValues
   }
 }
+
+object categories {
+  import enumeratum._
+
+  sealed abstract class Category(override val entryName: String) extends EnumEntry
+
+  object Category extends Enum[Category] {
+    case object Movie extends Category("Кино")
+
+    val values = findValues
+  }
+
+  // For validating path param in http4s routes
+  object CategoryVar {
+    def unapply(arg: String): Option[Category] = Category.withNameInsensitiveOption(arg)
+  }
+}
