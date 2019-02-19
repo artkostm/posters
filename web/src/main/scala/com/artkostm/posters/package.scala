7ab8ced6030dfaa0821584cf162462a94cd6fa81
Artsiom Chuiko <artkostm@gmail.com>
2019-02-19 15:02:51 +0300

    add root package object

---
+++ web/src/main/scala/com/artkostm/posters/package.scala
@@ -0,0 +1,12 @@
+package com.artkostm
+import java.time.ZoneId
+import java.time.format.DateTimeFormatterBuilder
+import java.time.temporal.ChronoField
+
+package object posters {
+  val FMT = new DateTimeFormatterBuilder()
+    .appendPattern("yyyy/MM/dd")
+    .parseDefaulting(ChronoField.NANO_OF_DAY, 0)
+    .toFormatter()
+    .withZone(ZoneId.of("Europe/Minsk"))
+}
