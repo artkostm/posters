858c7bf8ec48dc1eff4e96bfabc880174916bfda
Artsiom Chuiko <artkostm@gmail.com>
2019-02-19 15:03:43 +0300

    move value codecs to separate place

--- web/src/main/scala/com/artkostm/posters/jsoniter/package.scala
+++ web/src/main/scala/com/artkostm/posters/jsoniter/package.scala
@@ -1,3 +1,12 @@
 package com.artkostm.posters
+import com.artkostm.posters.interfaces.event.EventInfo
+import com.artkostm.posters.interfaces.schedule.Category
+import com.github.plokhotnyuk.jsoniter_scala.core.JsonValueCodec
+import com.github.plokhotnyuk.jsoniter_scala.macros.{CodecMakerConfig, JsonCodecMaker}
 
-package object jsoniter extends JsoniterInstances {}
+package object jsoniter extends JsoniterInstances {
+  object ValueCodecs {
+    implicit val eventInfoCodec: JsonValueCodec[EventInfo] = JsonCodecMaker.make[EventInfo](CodecMakerConfig())
+    implicit val categoryCodec: JsonValueCodec[Category] = JsonCodecMaker.make[Category](CodecMakerConfig())
+  }
+}
