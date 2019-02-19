21f714468b13277378c0735ac06ec6da2ae8da93
Artsiom Chuiko <artkostm@gmail.com>
2019-02-19 15:33:32 +0300

    update category names and move them to separate file

--- internal/src/main/scala/com/artkostm/posters/environments.scala
+++ internal/src/main/scala/com/artkostm/posters/environments.scala
@@ -12,20 +12,3 @@ object environments {
     val values = findValues
   }
 }
-
-object categories {
-  import enumeratum._
-
-  sealed abstract class Category(override val entryName: String) extends EnumEntry
-
-  object Category extends Enum[Category] {
-    case object Movie extends Category("Кино")
-
-    val values = findValues
-  }
-
-  // For validating path param in http4s routes
-  object CategoryVar {
-    def unapply(arg: String): Option[Category] = Category.withNameInsensitiveOption(arg)
-  }
-}
