b5a6976a509213932578402d63d5ca9d220711bd
Artsiom <artkostm@gmail.com>
2019-02-20 20:58:49 +0300

    add visitor endpoint initial implementation

--- web/src/main/scala/com/artkostm/posters/endpoint/VisitorEndpoint.scala
+++ web/src/main/scala/com/artkostm/posters/endpoint/VisitorEndpoint.scala
@@ -1,5 +1,25 @@
 package com.artkostm.posters.endpoint
 
-class VisitorEndpoint {
+import cats.data.EitherT
+import cats.implicits._
+import cats.effect.Effect
+import com.artkostm.posters.algebra.VisitorStore
+import com.artkostm.posters.interfaces.auth.User
+import com.artkostm.posters.interfaces.intent.Intent
+import org.http4s.AuthedService
+import org.http4s.dsl.Http4sDsl
 
+class VisitorEndpoint[F[_]: Effect](repository: VisitorStore[F]) extends Http4sDsl[F] {
+  import com.artkostm.posters.jsoniter._
+  import ValueCodecs._
+  import com.artkostm.posters.ValidationError._
+
+  private def saveVisitors(): AuthedService[User, F] = AuthedService {
+    case authed @ POST -> Root / "visitors" as _ =>
+      for {
+        intent  <- authed.req.as[Intent]
+        created <- EitherT.liftF(repository.save(intent)).value
+        resp    <- created.fold(BadRequest(_), Created(_))
+      } yield resp
+  }
 }
