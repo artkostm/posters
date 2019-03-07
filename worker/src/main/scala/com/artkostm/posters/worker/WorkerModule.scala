535323dc1a28c2e12918f7a7fc01fa7c90003f5d
Artsiom Chuiko <artkostm@gmail.com>
2019-03-07 16:13:14 +0300

    add test implementation of info store interpreter

--- worker/src/main/scala/com/artkostm/posters/worker/WorkerModule.scala
+++ worker/src/main/scala/com/artkostm/posters/worker/WorkerModule.scala
@@ -2,15 +2,17 @@ package com.artkostm.posters.worker
 
 import cats.effect._
 import com.artkostm.posters.Configuration.DatabaseConfig
+import com.artkostm.posters.algebra.InfoStore
+import com.artkostm.posters.interfaces.event.EventInfo
 import com.artkostm.posters.interpreter.{EventStoreInterpreter, InfoStoreInterpreter, VisitorStoreInterpreter}
-import com.artkostm.posters.scraper.AfishaScraper
+import com.artkostm.posters.scraper.{AfishaScraper, Scraper}
 import com.artkostm.posters.worker.collector.EventCollector
 import com.artkostm.posters.worker.config.{AppConfig, AppConfiguration}
 import com.artkostm.posters.worker.migration.DoobieMigration
 import doobie.hikari.HikariTransactor
 
 class WorkerModule[F[_]: Timer: Concurrent](config: AppConfig, val xa: HikariTransactor[F]) {
-  private lazy val scraper      = new AfishaScraper[F](config.scraper)
+  private lazy val scraper      = Scraper(config.scraper)
   private lazy val infoStore    = new InfoStoreInterpreter(xa.trans)
   private lazy val eventStore   = new EventStoreInterpreter(xa.trans)
   private lazy val visitorStore = new VisitorStoreInterpreter(xa.trans)
@@ -24,4 +26,19 @@ object WorkerModule {
       _      <- Resource.liftF(DoobieMigration.run[F](config))
       xa     <- DatabaseConfig.transactor(config.db)
     } yield new WorkerModule[F](config, xa)
+}
+
+class ISI[F[_]: Sync] extends InfoStore[F] {
+  override def deleteOld(): F[Int] = Sync[F].delay {
+    println("deleting old event info")
+    0
+  }
+  override def save(info: EventInfo): F[EventInfo] = Sync[F].delay {
+    println(s"saving event info $info")
+    info
+  }
+  override def find(link: String): F[Option[EventInfo]] = Sync[F].delay {
+    println(s"searching event info using $link")
+    None
+  }
 }
