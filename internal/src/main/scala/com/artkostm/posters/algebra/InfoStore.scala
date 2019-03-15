c4d0776df164ec33aca6038bd9a32ff6782a1406
Artsiom Chuiko <artkostm@gmail.com>
2019-03-15 18:52:15 +0300

    add bulk interface

--- internal/src/main/scala/com/artkostm/posters/algebra/InfoStore.scala
+++ internal/src/main/scala/com/artkostm/posters/algebra/InfoStore.scala
@@ -5,5 +5,6 @@ import com.artkostm.posters.interfaces.event.EventInfo
 trait InfoStore[F[_]] {
   def deleteOld(): F[Int]
   def save(info: EventInfo): F[EventInfo]
+  def save(info: List[EventInfo]): F[Int]
   def find(link: String): F[Option[EventInfo]]
 }
