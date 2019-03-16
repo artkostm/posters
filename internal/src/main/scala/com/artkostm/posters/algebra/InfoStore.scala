586f467ca1a7a02799c8d2622d1b8296162df156
Artsiom <artkostm@gmail.com>
2019-03-16 18:09:40 +0300

    update bulk save for info store

--- internal/src/main/scala/com/artkostm/posters/algebra/InfoStore.scala
+++ internal/src/main/scala/com/artkostm/posters/algebra/InfoStore.scala
@@ -1,10 +1,11 @@
 package com.artkostm.posters.algebra
 
-import com.artkostm.posters.interfaces.event.EventInfo
+import cats.Foldable
+import com.artkostm.posters.interfaces.event.{EventData, EventInfo}
 
 trait InfoStore[F[_]] {
   def deleteOld(): F[Int]
   def save(info: EventInfo): F[EventInfo]
-  def save(info: List[EventInfo]): F[Int]
+  def save[K[_]: Foldable](info: K[(String, EventData, EventData)]): F[Int]
   def find(link: String): F[Option[EventInfo]]
 }
