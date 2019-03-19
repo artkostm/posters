8cf4453f5f8ad1ff35d837b163742e213c24ebd8
Artsiom Chuiko <artkostm@gmail.com>
2019-03-19 17:35:43 +0300

    refactoring of visitor store

--- internal/src/main/scala/com/artkostm/posters/algebra/VisitorStore.scala
+++ internal/src/main/scala/com/artkostm/posters/algebra/VisitorStore.scala
@@ -10,20 +10,22 @@ trait VisitorStore[F[_]] {
   def find(date: Instant, eventName: String): F[Option[Intents]]
   def asVolunteer(intent: Intent): F[Intents]
   def asPlainUser(intent: Intent): F[Intents]
+  def leave(intent: Intent): F[Intents]
 }
 
 //object testJson extends App {
 //  println(Instant.now)
 //  import com.github.plokhotnyuk.jsoniter_scala.core._
+//  import com.github.plokhotnyuk.jsoniter_scala.macros.{CodecMakerConfig, JsonCodecMaker}
 //
 //  implicit val intentCodec: JsonValueCodec[Intent] = JsonCodecMaker.make[Intent](CodecMakerConfig())
 //
 //  val intent: Intent = readFromArray(
 //    """
-//      |{"date":"2019-02-19T13:56:24.678Z", "event_name":"event name"}
+//      |{"date":"2019-02-19T13:56:24.678Z", "eventName":"event name","userId":"2"}
 //    """.stripMargin.getBytes)
 //
 //  println(intent)
 //
-//  println(new String(writeToArray(Intent(Instant.now, "event name"))))
+//  println(new String(writeToArray(Intent(Instant.now, "event name", "1"))))
 //}
--- internal/src/main/scala/com/artkostm/posters/interpreter/VisitorStoreInterpreter.scala
+++ internal/src/main/scala/com/artkostm/posters/interpreter/VisitorStoreInterpreter.scala
@@ -10,37 +10,61 @@ import doobie.implicits._
 import doobie.postgres.implicits._
 
 class VisitorStoreInterpreter[F[_]](T: ConnectionIO ~> F) extends VisitorStore[F] {
+  import VisitorSQL._
+
   override def deleteOld(today: Instant): F[Int] =
-    T(sql"""DELETE FROM visitors WHERE date < $today""".update.run)
+    T(deleteOldIntents(today).run)
 
   override def save(intent: Intent): F[Intents] =
-    T(
-      sql"""INSERT INTO visitors ("date", "event_name", "vids", "uids") (${intent.date}, ${intent.eventName}, '{}', '{}')""".update
-        .withUniqueGeneratedKeys[Intents]("date", "event_name", "vids", "uids"))
+    T(saveIntent(intent).withUniqueGeneratedKeys[Intents]("date", "eventName", "vids", "uids"))
 
   override def find(date: Instant, eventName: String): F[Option[Intents]] =
-    T(
-      sql"""SELECT "date", event_name, vids, uids FROM visitors WHERE "date" = $date AND event_name = $eventName"""
-        .query[Intents]
-        .option)
+    T(findByDateAndName(date, eventName).option)
 
   override def asVolunteer(intent: Intent): F[Intents] =
-    T(
-      sql"""
-           |INSERT INTO visitors ("date", "event_name", "vids", "uids") (${intent.date}, ${intent.eventName}, '{${intent.userId}}', '{}')
-           |ON CONFLICT ON CONSTRAINT pk_visitors
-           |DO
-           |UPDATE visitors SET vids = array_append(vids, ${intent.userId}) WHERE event_name = ${intent.eventName} AND "date" = ${intent.date}
-           |""".stripMargin.update
-        .withUniqueGeneratedKeys[Intents]("date", "event_name", "vids", "uids"))
+    T(volunteer(intent).withUniqueGeneratedKeys[Intents]("date", "eventName", "vids", "uids"))
 
   override def asPlainUser(intent: Intent): F[Intents] =
-    T(
-      sql"""
-           |INSERT INTO visitors ("date", "event_name", "vids", "uids") (${intent.date}, ${intent.eventName}, '{}', '{${intent.userId}}')
-           |ON CONFLICT ON CONSTRAINT pk_visitors
-           |DO
-           |UPDATE visitors SET uids = array_append(uids, ${intent.userId}) WHERE event_name = ${intent.eventName} AND "date" = ${intent.date}
-           |""".stripMargin.update
-        .withUniqueGeneratedKeys[Intents]("date", "event_name", "vids", "uids"))
+    T(plainUser(intent).withUniqueGeneratedKeys[Intents]("date", "eventName", "vids", "uids"))
+
+  override def leave(intent: Intent): F[Intents] =
+    T(leaveEvent(intent).withUniqueGeneratedKeys[Intents]("date", "eventName", "vids", "uids"))
+}
+
+private object VisitorSQL {
+  def findByDateAndName(date: Instant, eventName: String): Query0[Intents] =
+    sql"""SELECT "date", eventName, vids, uids FROM visitors WHERE "date" = $date AND eventName = $eventName"""
+      .query[Intents]
+
+  def saveIntent(intent: Intent): Update0 =
+    sql"""
+         INSERT INTO visitors ("date", "eventName", "vids", "uids")
+         (${intent.date}, ${intent.eventName}, '{}', '{}')""".update
+
+  def deleteOldIntents(today: Instant): Update0 =
+    sql"""DELETE FROM visitors WHERE date < $today""".update
+
+  def leaveEvent(intent: Intent): Update0 =
+    sql"""
+         UPDATE visitors
+         SET uids = array_remove(uids, ${intent.userId}), vids = array_remove(vids, ${intent.userId})
+         WHERE eventName = ${intent.eventName} AND "date" = ${intent.date}
+         """.update
+
+  def whereRf(intent: Intent) = fr"""WHERE eventName = ${intent.eventName} AND "date" = ${intent.date}"""
+
+  def createOrUpdate(intent: Intent, insertFr: Fragment, updateFr: Fragment): Fragment =
+    fr"""INSERT INTO visitors ("date", "eventName", "vids", "uids")""" ++ insertFr ++
+      fr"ON CONFLICT ON CONSTRAINT pk_visitors DO UPDATE visitors SET" ++ updateFr ++
+      whereRf(intent)
+
+  def volunteer(intent: Intent): Update0 =
+    createOrUpdate(intent,
+      fr"(${intent.date}, ${intent.eventName}, '{${intent.userId}}', '{}')",
+      fr"vids = array_append(vids, ${intent.userId})").update
+
+  def plainUser(intent: Intent): Update0 =
+    createOrUpdate(intent,
+      fr"(${intent.date}, ${intent.eventName}, '{}', '{${intent.userId}}')",
+      fr"uids = array_append(uids, ${intent.userId})").update
 }
