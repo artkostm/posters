854b73e221b93df1aa8e098fe1bbd8a5b1eea20f
Artsiom Chuiko <artkostm@gmail.com>
2019-03-29 16:18:16 +0300

    jsoniter instances update

---
+++ internal/src/main/scala/com/artkostm/posters/jsoniter/JsoniterInstances.scala
@@ -0,0 +1,36 @@
+package com.artkostm.posters.jsoniter
+
+import java.nio.ByteBuffer
+
+import cats._
+import cats.effect._
+import com.github.plokhotnyuk.jsoniter_scala.core._
+import org.http4s.{DecodeResult, EntityDecoder, EntityEncoder, MalformedMessageBodyFailure, MediaType}
+import org.http4s.headers.`Content-Type`
+
+import scala.util.Try
+
+trait JsoniterInstances {
+  implicit def jsoniterEntityEncoder[F[_]: Applicative, A: JsonValueCodec]: EntityEncoder[F, A] =
+    EntityEncoder
+      .byteArrayEncoder[F]
+      .contramap[A](writeToArray(_))
+      .withContentType(`Content-Type`(MediaType.application.`json`))
+
+  implicit def jsoniterEntityDecoder[F[_]: Sync, A: JsonValueCodec]: EntityDecoder[F, A] =
+    EntityDecoder.decodeBy(MediaType.application.`json`) { msg =>
+      EntityDecoder.collectBinary(msg).flatMap { segment =>
+        val bb = ByteBuffer.wrap(segment.toArray)
+        if (bb.hasRemaining) {
+          Try(readFromArray(bb.array())).toEither match {
+            case Right(json) =>
+              DecodeResult.success[F, A](json)
+            case Left(pf) =>
+              DecodeResult.failure[F, A](MalformedMessageBodyFailure("Invalid JSON", Some(pf)))
+          }
+        } else {
+          DecodeResult.failure[F, A](MalformedMessageBodyFailure("Invalid JSON: empty body", None))
+        }
+      }
+    }
+}
