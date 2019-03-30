74f6c13212bcd9ca5a017bcfd8a58569520ccd78
Artsiom <artkostm@gmail.com>
2019-03-30 15:25:23 +0300

    update event store interpreter

--- internal/src/main/scala/com/artkostm/posters/interpreter/EventStoreInterpreter.scala
+++ internal/src/main/scala/com/artkostm/posters/interpreter/EventStoreInterpreter.scala
@@ -1,6 +1,6 @@
 package com.artkostm.posters.interpreter
 
-import java.time.Instant
+import java.time.LocalDate
 
 import cats.data._
 import cats.~>
@@ -15,13 +15,13 @@ import doobie.util.fragments
 class EventStoreInterpreter[F[_]](T: ConnectionIO ~> F) extends EventStore[F] {
   import EventStoreInterpreter._
 
-  //implicit val dayJsonValueCodec = dayCodec
+  implicit val dayJsonValueCodec = dayCodec
 
   override def save(day: Day): F[Day] =
-    T(saveEvents(day).withUniqueGeneratedKeys[Day]("date", "categories"))
+    T(saveEvents(day).withUniqueGeneratedKeys[Day]("eventdate", "categories"))
 
-  override def findByDate(day: Instant): F[Option[Day]] =
-    T(sql"""SELECT "date", "categories" FROM events WHERE date=$day""".query[Day].option)
+  override def findByDate(day: LocalDate): F[Option[Day]] =
+    T(sql"""SELECT "eventdate", "categories" FROM events e WHERE e.eventdate=$day""".query[Day].option)
 
   override def findByNames(names: NonEmptyList[String]): F[List[Category]] =
     T(
@@ -31,22 +31,22 @@ class EventStoreInterpreter[F[_]](T: ConnectionIO ~> F) extends EventStore[F] {
       ).query[Category]
         .to[List])
 
-  override def findByNamesAndPeriod(names: NonEmptyList[String], start: Instant, end: Instant): F[List[Category]] =
+  override def findByNamesAndPeriod(names: NonEmptyList[String], start: LocalDate, end: LocalDate): F[List[Category]] =
     T(
       (
-        fr"""SELECT j FROM events t, jsonb_array_elements(t.categories) j WHERE t.date > $start AND t.date < $end AND""" ++ fragments
+        fr"""SELECT j FROM events t, jsonb_array_elements(t.categories) j WHERE t.eventdate > $start AND t.eventdate < $end AND""" ++ fragments
           .in(fr"""j->>'name'""", names)
       ).query[Category]
         .to[List])
 
-  override def findByNameAndDate(name: String, date: Instant): F[Option[Category]] =
+  override def findByNameAndDate(name: String, date: LocalDate): F[Option[Category]] =
     T(
-      sql"""SELECT j FROM events t, jsonb_array_elements(t.categories) j WHERE j->>'name' = $name AND t.date = $date"""
+      sql"""SELECT j FROM events t, jsonb_array_elements(t.categories) j WHERE j->>'name' = $name AND t.eventdate = $date"""
         .query[Category]
         .option)
 
-  override def deleteOld(today: Instant): F[Int] =
-    T(sql"""DELETE FROM events WHERE date < $today""".update.run)
+  override def deleteOld(today: LocalDate): F[Int] =
+    T(sql"""DELETE FROM events WHERE events.eventdate < $today""".update.run)
 
 }
 
@@ -54,6 +54,6 @@ private object EventStoreInterpreter {
   implicit val han = LogHandler.jdkLogHandler
 
   def saveEvents(day: Day): Update0 =
-    sql"""INSERT INTO events ("date", "categories") VALUES (${day.date}, ${day.categories})
-         ON CONFLICT ON CONSTRAINT pk_events DO UPDATE SET "categories"=${day.categories} """.update
+    sql"""INSERT INTO events ("eventdate", "categories") VALUES (${day.eventDate}, ${day.categories})
+         ON CONFLICT ON CONSTRAINT pk_events DO UPDATE SET categories=${day.categories} """.update
 }
