3b56939f8f1dcc12858c05943412a89d5fbabbf4
Artsiom Chuiko <artkostm@gmail.com>
2019-04-01 18:22:58 +0300

    update visitor endpoint

--- web/src/main/scala/com/artkostm/posters/endpoint/VisitorEndpoint.scala
+++ web/src/main/scala/com/artkostm/posters/endpoint/VisitorEndpoint.scala
@@ -1,16 +1,14 @@
 package com.artkostm.posters.endpoint
 
-import cats.data.EitherT
 import cats.implicits._
 import cats.effect.Effect
-import com.artkostm.posters.algebra.VisitorStore
-import com.artkostm.posters.endpoint.auth.role.Role
 import com.artkostm.posters.interfaces.auth.User
 import com.artkostm.posters.interfaces.intent.{Intent, Intents}
+import com.artkostm.posters.service.VisitorsService
 import org.http4s.AuthedService
 import org.http4s.dsl.Http4sDsl
 
-class VisitorEndpoint[F[_]: Effect](repository: VisitorStore[F]) extends Http4sDsl[F] with EndpointsAware[F] {
+class VisitorEndpoint[F[_]: Effect](service: VisitorsService[F]) extends Http4sDsl[F] with EndpointsAware[F] {
   import com.artkostm.posters.jsoniter._
   import com.artkostm.posters.jsoniter.codecs._
   import com.artkostm.posters.ValidationError._
@@ -19,7 +17,7 @@ class VisitorEndpoint[F[_]: Effect](repository: VisitorStore[F]) extends Http4sD
     case authed @ POST -> Root / "visitors" as User(_, role) =>
       for {
         intent  <- authed.req.as[Intent]
-        created <- Role.on[F, Intents](role, repository.asPlainUser(intent), repository.asVolunteer(intent)).value
+        created <- service.saveIntent(role, intent).value
         resp    <- created.fold(BadRequest(_), Created(_))
       } yield resp
   }
@@ -28,6 +26,6 @@ class VisitorEndpoint[F[_]: Effect](repository: VisitorStore[F]) extends Http4sD
 }
 
 object VisitorEndpoint {
-  def apply[F[_]: Effect](repository: VisitorStore[F]): AuthedService[User, F] =
-    new VisitorEndpoint(repository).endpoints
+  def apply[F[_]: Effect](service: VisitorsService[F]): AuthedService[User, F] =
+    new VisitorEndpoint(service).endpoints
 }
