e8f293d8df3ba6ae1ac6dbd8f25900b43f0023d1
Artsiom Chuiko <artkostm@gmail.com>
2019-04-01 18:22:41 +0300

    update web module

--- web/src/main/scala/com/artkostm/posters/WebModule.scala
+++ web/src/main/scala/com/artkostm/posters/WebModule.scala
@@ -10,15 +10,20 @@ import cats.syntax.semigroupk._
 import cats.syntax.functor._
 import com.olegpy.meow.hierarchy._
 import com.artkostm.posters.Configuration.DatabaseConfig
+import com.artkostm.posters.service.VisitorsService
 
 class WebModule[F[_]: Effect](val config: AppConfig, val xa: HikariTransactor[F]) {
 
-  private lazy val infoStore    = new InfoStoreInterpreter(xa.trans)
-  private lazy val eventStore   = new EventStoreInterpreter(xa.trans)
-  private lazy val visitorStore = new VisitorStoreInterpreter(xa.trans)
-  lazy val scraper: Scraper[F]  = new AfishaScraper[F](config.scraper)
+  private lazy val infoStore      = new InfoStoreInterpreter(xa.trans)
+  private lazy val eventStore     = new EventStoreInterpreter(xa.trans)
+  private lazy val visitorStore   = new VisitorStoreInterpreter(xa.trans)
+
+  private lazy val visitorService = new VisitorsService[F](visitorStore)
+
+  lazy val scraper: Scraper[F]    = new AfishaScraper[F](config.scraper)
+
   lazy val endpoints = InfoEndpoint[F](infoStore) <+> CategoryEndpoint[F](eventStore, scraper) <+> VisitorEndpoint[F](
-    visitorStore)
+    visitorService)
 }
 
 object WebModule {
