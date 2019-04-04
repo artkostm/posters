b909f346572534d501cac7863fc64f46bed5f28d
Artsiom Chuiko <artkostm@gmail.com>
2019-04-04 17:37:18 +0300

    update visitor endpoint

--- web/src/main/scala/com/artkostm/posters/endpoint/VisitorEndpoint.scala
+++ web/src/main/scala/com/artkostm/posters/endpoint/VisitorEndpoint.scala
@@ -3,7 +3,7 @@ package com.artkostm.posters.endpoint
 import cats.implicits._
 import cats.effect.Effect
 import com.artkostm.posters.interfaces.auth.User
-import com.artkostm.posters.interfaces.intent.{Intent, Intents}
+import com.artkostm.posters.interfaces.intent.Intent
 import com.artkostm.posters.service.VisitorsService
 import org.http4s.AuthedService
 import org.http4s.dsl.Http4sDsl
@@ -17,12 +17,22 @@ class VisitorEndpoint[F[_]: Effect](service: VisitorsService[F]) extends Http4sD
     case authed @ POST -> Root / "visitors" as User(_, role) =>
       for {
         intent  <- authed.req.as[Intent]
-        created <- service.saveIntent(role, intent).value
+        created <- service.saveOrUpdateIntent(role, intent).value
         resp    <- created.fold(BadRequest(_), Created(_))
       } yield resp
   }
 
-  override def endpoints: AuthedService[User, F] = saveVisitors()
+  private def leaveEvent(): AuthedService[User, F] = AuthedService {
+    case authed @ DELETE -> Root / "visitors" as _ =>
+      for {
+        intent  <- authed.req.as[Intent]
+        updated <- service.leaveEvent(intent).value
+        // TODO: update error handling with correct http status codes
+        resp    <- updated.fold(BadRequest(_), Ok(_))
+      } yield resp
+  }
+
+  override def endpoints: AuthedService[User, F] = saveVisitors() <+> leaveEvent()
 }
 
 object VisitorEndpoint {
