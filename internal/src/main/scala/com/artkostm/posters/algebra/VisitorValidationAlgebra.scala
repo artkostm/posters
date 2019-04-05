6cf10c9eeea55f07bdf9d8e52d3530f576e8ec0f
Artsiom Chuiko <artkostm@gmail.com>
2019-04-05 14:53:21 +0300

    small fixes

--- internal/src/main/scala/com/artkostm/posters/algebra/VisitorValidationAlgebra.scala
+++ internal/src/main/scala/com/artkostm/posters/algebra/VisitorValidationAlgebra.scala
@@ -2,8 +2,8 @@ package com.artkostm.posters.algebra
 
 import cats.data.EitherT
 import com.artkostm.posters.ValidationError.IntentDoesNotExistError
-import com.artkostm.posters.interfaces.intent.Intent
+import com.artkostm.posters.interfaces.intent.{Intent, Intents}
 
 trait VisitorValidationAlgebra[F[_]] {
-  def exists(intent: Intent): EitherT[F, IntentDoesNotExistError, Unit]
+  def exists(intent: Intent): EitherT[F, IntentDoesNotExistError, Intents]
 }
--- internal/src/main/scala/com/artkostm/posters/interpreter/VisitorValidationInterpreter.scala
+++ internal/src/main/scala/com/artkostm/posters/interpreter/VisitorValidationInterpreter.scala
@@ -4,10 +4,10 @@ import cats.Functor
 import cats.data.EitherT
 import com.artkostm.posters.ValidationError.IntentDoesNotExistError
 import com.artkostm.posters.algebra.{VisitorStore, VisitorValidationAlgebra}
-import com.artkostm.posters.interfaces.intent.Intent
+import com.artkostm.posters.interfaces.intent.{Intent, Intents}
 
 class VisitorValidationInterpreter[F[_]: Functor](store: VisitorStore[F]) extends VisitorValidationAlgebra[F] {
-  override def exists(intent: Intent): EitherT[F, IntentDoesNotExistError, Unit] =
+  override def exists(intent: Intent): EitherT[F, IntentDoesNotExistError, Intents] =
     EitherT.fromOptionF(
       store.find(intent.eventDate, intent.eventName),
       IntentDoesNotExistError(intent.eventName, intent.eventDate)
