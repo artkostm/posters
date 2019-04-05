41ab39eec1f75962075059e96f37fa261b0dc01c
Artsiom Chuiko <artkostm@gmail.com>
2019-04-05 13:11:24 +0300

    update visitor validation alg

--- internal/src/main/scala/com/artkostm/posters/interpreter/VisitorValidationInterpreter.scala
+++ internal/src/main/scala/com/artkostm/posters/interpreter/VisitorValidationInterpreter.scala
@@ -2,15 +2,14 @@ package com.artkostm.posters.interpreter
 
 import cats.Functor
 import cats.data.EitherT
-import com.artkostm.posters.ValidationError.ApiError
+import com.artkostm.posters.ValidationError.IntentDoesNotExistError
 import com.artkostm.posters.algebra.{VisitorStore, VisitorValidationAlgebra}
 import com.artkostm.posters.interfaces.intent.Intent
 
 class VisitorValidationInterpreter[F[_]: Functor](store: VisitorStore[F]) extends VisitorValidationAlgebra[F] {
-  override def exists(intent: Intent): EitherT[F, ApiError, Unit] =
+  override def exists(intent: Intent): EitherT[F, IntentDoesNotExistError, Unit] =
     EitherT.fromOptionF(
       store.find(intent.eventDate, intent.eventName),
-      // TODO: change error type
-      ApiError(s"Can't find '${intent.eventName}' event on ${intent.eventDate}", 404)
+      IntentDoesNotExistError(intent.eventName, intent.eventDate)
     )
 }
