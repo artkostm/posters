6f2343b9fef9b4dc7f895f92ae1eda8d8ced8bd1
Artsiom Chuiko <artkostm@gmail.com>
2019-04-05 13:11:41 +0300

    update visitor service

--- web/src/main/scala/com/artkostm/posters/service/VisitorsService.scala
+++ web/src/main/scala/com/artkostm/posters/service/VisitorsService.scala
@@ -2,22 +2,21 @@ package com.artkostm.posters.service
 
 import cats.{Applicative, Monad}
 import cats.data.EitherT
-import com.artkostm.posters.ValidationError.ApiError
+import com.artkostm.posters.ValidationError.{IntentDoesNotExistError, RoleDoesNotExistError}
 import com.artkostm.posters.algebra.{VisitorStore, VisitorValidationAlgebra}
 import com.artkostm.posters.endpoint.auth.role.Role
 import com.artkostm.posters.endpoint.auth.role.Role.{User, Volunteer}
 import com.artkostm.posters.interfaces.intent.{Intent, Intents}
 
 class VisitorsService[F[_]: Monad](repository: VisitorStore[F], validator: VisitorValidationAlgebra[F]) {
-  // TODO: change error type
-  def saveOrUpdateIntent(role: String, intent: Intent): EitherT[F, ApiError, Intents] =
+  def saveOrUpdateIntent(role: String, intent: Intent): EitherT[F, RoleDoesNotExistError, Intents] =
     Role.withNameInsensitiveOption(role) match {
       case Some(User)      => EitherT.liftF(repository.asPlainUser(intent))
       case Some(Volunteer) => EitherT.liftF(repository.asVolunteer(intent))
-      case _               => EitherT.leftT(Applicative[F].pure(ApiError(s"There is no $role.", 400)))
+      case _               => EitherT.leftT(Applicative[F].pure(RoleDoesNotExistError(role)))
     }
 
-  def leaveEvent(intent: Intent): EitherT[F, ApiError, Intents] =
+  def leaveEvent(intent: Intent): EitherT[F, IntentDoesNotExistError, Intents] =
     for {
       _       <- validator.exists(intent)
       updated <- EitherT.liftF(repository.leave(intent))
