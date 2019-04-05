b842a6cb07830428dea127096d7f893dd7288d2b
Artsiom Chuiko <artkostm@gmail.com>
2019-04-05 14:38:47 +0300

    update errors

--- internal/src/main/scala/com/artkostm/posters/ValidationError.scala
+++ internal/src/main/scala/com/artkostm/posters/ValidationError.scala
@@ -2,9 +2,6 @@ package com.artkostm.posters
 
 import java.time.LocalDate
 
-import com.github.plokhotnyuk.jsoniter_scala.core.JsonValueCodec
-import com.github.plokhotnyuk.jsoniter_scala.macros.{CodecMakerConfig, JsonCodecMaker}
-
 sealed trait ValidationError extends Throwable
 
 object ValidationError {
@@ -13,9 +10,4 @@ object ValidationError {
   final case class EventInfoNotFoundError(link: String) extends ValidationError
   final case class RoleDoesNotExistError(role: String) extends ValidationError
   final case class IntentDoesNotExistError(eventName: String, date: LocalDate) extends ValidationError
-
-  final case class ApiError(msg: String, code: Int)
-
-  implicit val apiErrorCodec: JsonValueCodec[ApiError] =
-    JsonCodecMaker.make[ApiError](CodecMakerConfig())
 }
--- web/src/main/scala/com/artkostm/posters/endpoint/error/HttpErrorHandler.scala
+++ web/src/main/scala/com/artkostm/posters/endpoint/error/HttpErrorHandler.scala
@@ -4,15 +4,16 @@ import cats.Monad
 import com.artkostm.posters.jsoniter._
 import com.artkostm.posters.ValidationError
 import com.artkostm.posters.ValidationError._
+import com.artkostm.posters.endpoint.ApiError
 import org.http4s.Response
 import org.http4s.dsl.Http4sDsl
 
 class HttpErrorHandler[F[_]: Monad] extends Http4sDsl[F] {
 
-  val handler: ValidationError => F[Response[F]] = {
-    case CategoryNotFoundError(name, date)   => NotFound(ApiError("", 3))
-    case CategoriesNotFoundError(date)       => NotFound("")
-    case EventInfoNotFoundError(link)        => NotFound("")
+  val handle: ValidationError => F[Response[F]] = {
+    case CategoryNotFoundError(name, date)   => NotFound(ApiError(s"Cannot find '$name' category using date=$date", 404))
+    case CategoriesNotFoundError(date)       => NotFound(ApiError(s"Cannot find categories using date=$date", 404))
+    case EventInfoNotFoundError(link)        => NotFound(ApiError(s"Cannot find event using $link", 404))
     case RoleDoesNotExistError(role)         => BadRequest(ApiError(s"There is no $role.", 400))
     case IntentDoesNotExistError(name, date) => NotFound(ApiError(s"Can't find '$name' event on $date", 404))
   }
