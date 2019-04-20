a79178cd069f6ceb5f37676e45da3b9f39ef8b41
Artsiom <artkostm@gmail.com>
2019-04-20 20:55:27 +0300

    update main

--- web/src/main/scala/com/artkostm/posters/Main.scala
+++ web/src/main/scala/com/artkostm/posters/Main.scala
@@ -18,11 +18,12 @@ object Main extends IOApp {
 
   def server(): Resource[IO, Server[IO]] =
     for {
-      module <- WebModule.init
+      module    <- WebModule.init
+      endpoints <- Resource.liftF(module.endpoints.value)
       server <- BlazeServerBuilder[IO]
                  .withNio2(true)
                  .bindHttp(module.config.http.port.value, "0.0.0.0")
-                 .withHttpApp(Router("/" -> module.endpoints).orNotFound)
+                 .withHttpApp(Router("/" -> endpoints.orNull).orNotFound)
                  .resource
     } yield server
 }
@@ -31,16 +32,18 @@ object testMonocle extends App {
   val req = DialogflowRequest(
     "id",
     "lang",
-    Result("source",
-           "resolvedQuery",
-           None,
-           "action",
-           true,
-           Parameters(List(), Datetime(None, Some(Period(LocalDate.now, LocalDate.now)))),
-           List(),
-           Metadata("", "", "", ""),
-           Fulfillment("speech", List()),
-           1.0),
+    Result(
+      "source",
+      "resolvedQuery",
+      None,
+      "action",
+      true,
+      Parameters(List(), Datetime(None, Some(Period(LocalDate.now, LocalDate.now)))),
+      List(),
+      Metadata("", "", "", ""),
+      Fulfillment("speech", List()),
+      1.0
+    ),
     Status(1, "errorType", Some(true)),
     "sessionId"
   )
@@ -52,7 +55,8 @@ object testMonocle extends App {
   implicit val codec = JsonCodecMaker.make[DialogflowRequest](CodecMakerConfig())
 
 //  println(new String(writeToArray(req)))
-  println(readFromArray[DialogflowRequest]("""{"id":"id","lang":"lang","result":{"source":"source","resolvedQuery":"resolvedQuery","action":"action","actionIncomplete":true,"parameters":{"datetime":{"period":"2019-04-15/2019-04-13"}},"metadata":{"intentId":"","webhookUsed":"","webhookForSlotFillingUsed":"","intentName":""},"fulfillment":{"speech":"speech"},"score":1.0},"status":{"code":1,"errorType":"errorType","webhookTimedOut":true},"sessionId":"sessionId"}""".getBytes))
+  println(readFromArray[DialogflowRequest](
+    """{"id":"id","lang":"lang","result":{"source":"source","resolvedQuery":"resolvedQuery","action":"action","actionIncomplete":true,"parameters":{"datetime":{"period":"2019-04-15/2019-04-13"}},"metadata":{"intentId":"","webhookUsed":"","webhookForSlotFillingUsed":"","intentName":""},"fulfillment":{"speech":"speech"},"score":1.0},"status":{"code":1,"errorType":"errorType","webhookTimedOut":true},"sessionId":"sessionId"}""".getBytes))
 
 //  val pr = monocle.Prism.partial[DialogflowRequest, List[String]] {
 //    case DialogflowRequest(_, QueryResult(_, Parameters(category, _), _, _, _, _, _), _, _) => category
