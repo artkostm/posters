b73c171be9a883f8980574a1a2eccea4afac0204
Artsiom <artkostm@gmail.com>
2019-04-21 18:25:43 +0300

    introduce DfWebhookService

--- web/src/main/scala/com/artkostm/posters/service/DfWebhookService.scala
+++ web/src/main/scala/com/artkostm/posters/service/DfWebhookService.scala
@@ -1,3 +1,22 @@
-package com.artkostm.posters.service class DfWebhookService {
+package com.artkostm.posters.service
+
+import cats.data.NonEmptyList
+import com.artkostm.posters.algebra.EventStore
+import com.artkostm.posters.service.DfKeyData._
+
+class DfWebhookService[F[_]](repository: EventStore[F]) {
+
+  def execute[DfRequest: DfKeyData](request: DfRequest) = {
+    if (request.incomplete) {
+      request.date match {
+        case Some(date) =>
+          if (request.hasAllCategory) {
+            repository.findByDate(date)
+          } else {
+            repository.findByNamesAndDate(NonEmptyList.fromListUnsafe(request.categories), date)
+          }
+      }
+    }
+  }
 
 }
