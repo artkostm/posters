5d7ae482683502aaca263112b8b3ce9f79007dd3
Artsiom <artkostm@gmail.com>
2019-04-28 21:34:10 +0300

    update test

--- web/src/test/scala/com/artkostm/posters/endpoint/InfoEndpointTest.scala
+++ web/src/test/scala/com/artkostm/posters/endpoint/InfoEndpointTest.scala
@@ -1,39 +1,59 @@
 package com.artkostm.posters.endpoint
 
-import cats.Foldable
 import cats.effect.IO
 import com.artkostm.posters.algebra.InfoStore
 import com.artkostm.posters.endpoint.error.HttpErrorHandler
 import com.artkostm.posters.interfaces.auth.User
 import com.artkostm.posters.interfaces.event.{EventData, EventInfo}
+import org.http4s.dsl.Http4sDsl
 import org.http4s.{AuthedRequest, Method, Request, Uri}
 import org.http4s.implicits._
-import org.scalatest.FlatSpec
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.{FlatSpec, Matchers}
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 
-class InfoEndpointTest extends FlatSpec {
+class InfoEndpointTest
+    extends FlatSpec
+    with Matchers
+    with MockFactory
+    with ScalaCheckPropertyChecks
+    with Http4sDsl[IO]
+    with PostersArbitraries {
 
   implicit val errorHandler = new HttpErrorHandler[IO]
 
-  val infoRepo = new InfoStore[IO] {
-    override def deleteOld(): IO[Int] = IO.pure(1)
+//  val infoRepo = new InfoStore[IO] {
+//    override def deleteOld(): IO[Int] = IO.pure(1)
+//
+//    override def save(info: EventInfo): IO[EventInfo] = IO.pure(info)
+//
+//    override def save[K[_]: Foldable](info: K[(String, EventData, EventData)]): IO[Int] = IO.pure(1)
+//
+//    override def find(link: String): IO[Option[EventInfo]] =
+//      IO.pure(Some(EventInfo(link, EventData("description", List(), List()))))
+//  }
 
-    override def save(info: EventInfo): IO[EventInfo] = IO.pure(info)
+  "InfoEndpoint" should "return event info by link with 200 status code" in {
+    val infoRepo = mock[InfoStore[IO]]
 
-    override def save[K[_] : Foldable](info: K[(String, EventData, EventData)]): IO[Int] = IO.pure(1)
-
-    override def find(link: String): IO[Option[EventInfo]] = IO.pure(Some(EventInfo(link, EventData("description", List(), List()))))
-  }
-
-  "Endpoint" should "return event info by link" in {
     val infoEndpoint = new InfoEndpoint[IO](infoRepo)
 
-    val link = "https://test.io/img.jpg"
-
-    val responseIO = infoEndpoint.endpoints.orNotFound(AuthedRequest(User("apiKey", "role", "id"), Request[IO](Method.GET, Uri.unsafeFromString(s"/v1/events?link=$link"))))
-
-    val response = responseIO.unsafeRunSync()
-
-    println(new String(response.body.compile.toList.unsafeRunSync().toArray))
+    forAll { (link: Link, user: User) =>
+      givenInfoStoreReturnsEventInfo(infoRepo)
+      (
+        for {
+          response <- infoEndpoint.endpoints.orNotFound(
+                       AuthedRequest(user,
+                                     Request[IO](Method.GET, Uri.unsafeFromString(s"/v1/events?link=${link.value}"))))
+        } yield {
+          response.status shouldEqual Ok
+        }
+      ).unsafeRunSync()
+    }
   }
 
+  def givenInfoStoreReturnsEventInfo(store: InfoStore[IO]) =
+    (store.find _) expects (*) onCall { l: String =>
+      IO.pure(Some(EventInfo(l, EventData("description", List(), List()))))
+    }
 }
