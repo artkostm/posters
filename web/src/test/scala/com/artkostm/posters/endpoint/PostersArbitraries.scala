b47b91bb9b6d903e2ea96d306ef386ffe436fbe5
Artsiom <artkostm@gmail.com>
2019-04-29 22:18:21 +0300

    add a few df arbitraries of v1

--- web/src/test/scala/com/artkostm/posters/endpoint/PostersArbitraries.scala
+++ web/src/test/scala/com/artkostm/posters/endpoint/PostersArbitraries.scala
@@ -2,8 +2,10 @@ package com.artkostm.posters.endpoint
 
 import java.time.LocalDate
 
+import com.artkostm.posters.categories
 import com.artkostm.posters.endpoint.auth.role.Role
 import com.artkostm.posters.interfaces.auth.User
+import com.artkostm.posters.interfaces.dialog.v1.{ContextParams, Datetime, Parameters, Period}
 import com.artkostm.posters.interfaces.event.{Comment, EventData, EventInfo}
 import org.scalacheck.{Arbitrary, Gen}
 import org.scalacheck.Arbitrary.arbitrary
@@ -15,12 +17,6 @@ trait PostersArbitraries {
     for {
       link <- Gen.alphaStr
     } yield Link(link)
-  }
-
-  implicit val localDate = Arbitrary[LocalDate] {
-    for {
-      millis <- Gen.posNum[Long]
-    } yield LocalDate.ofEpochDay(millis)
   }
 
   implicit val comment = Arbitrary[Comment] {
@@ -55,6 +51,50 @@ trait PostersArbitraries {
       role   <- Gen.oneOf(Role.User.entryName, Role.Volunteer.entryName)
       id     <- Gen.alphaStr
     } yield User(apiKey, role, id)
+  }
+}
+
+trait DialogflowV1Arbitraries { this: BaseArbitraries =>
+
+  implicit val period1 = Arbitrary[Period] {
+    for {
+      start   <- arbitrary[LocalDate](this.localDate)
+      numDays <- Gen.choose(1, 20)
+    } yield Period(start.plusDays(numDays), start)
+  }
+
+  implicit val datetime1 = Arbitrary[Datetime] {
+    for {
+      date   <- Gen.option(arbitrary[LocalDate](this.localDate))
+      p      <- Gen.option(arbitrary[Period])
+      period = if (date.isDefined) None else p
+    } yield Datetime(date, period)
+  }
+
+  implicit val parameters = Arbitrary[Parameters] {
+    for {
+      numCategories <- Gen.choose(0, 8)
+      category      <- Gen.containerOfN[List, categories.Category](numCategories, Gen.oneOf(categories.Category.values))
+      datetime      <- arbitrary[Datetime]
+    } yield Parameters(category.map(_.entryName), datetime)
+  }
+
+  implicit val contextParams = Arbitrary[ContextParams] {
+    for {
+      datetime         <- Gen.alphaNumStr
+      datetimeOriginal <- Gen.alphaNumStr
+      numCategories    <- Gen.choose(0, 8)
+      category         <- Gen.containerOfN[List, categories.Category](numCategories, Gen.oneOf(categories.Category.values))
+    } yield
+      ContextParams(datetime, datetimeOriginal, category.map(_.entryName), category.map(_.entryName).mkString(","))
+  }
+}
+
+trait BaseArbitraries {
+  implicit val localDate = Arbitrary[LocalDate] {
+    for {
+      millis <- Gen.posNum[Long]
+    } yield LocalDate.ofEpochDay(millis)
   }
 }
 
