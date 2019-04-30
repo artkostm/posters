116a75a99d0fa52f178186ade5bf347b443b4882
Artsiom <artkostm@gmail.com>
2019-04-30 20:14:07 +0300

    add df (v1) arbitraries

--- web/src/test/scala/com/artkostm/posters/endpoint/PostersArbitraries.scala
+++ web/src/test/scala/com/artkostm/posters/endpoint/PostersArbitraries.scala
@@ -5,7 +5,19 @@ import java.time.LocalDate
 import com.artkostm.posters.categories
 import com.artkostm.posters.endpoint.auth.role.Role
 import com.artkostm.posters.interfaces.auth.User
-import com.artkostm.posters.interfaces.dialog.v1.{ContextParams, Datetime, Parameters, Period}
+import com.artkostm.posters.interfaces.dialog.v1.{
+  Context,
+  ContextParams,
+  Datetime,
+  DialogflowRequest,
+  Fulfillment,
+  Message,
+  Metadata,
+  Parameters,
+  Period,
+  Result,
+  Status
+}
 import com.artkostm.posters.interfaces.event.{Comment, EventData, EventInfo}
 import org.scalacheck.{Arbitrary, Gen}
 import org.scalacheck.Arbitrary.arbitrary
@@ -87,6 +99,82 @@ trait DialogflowV1Arbitraries { this: BaseArbitraries =>
       category         <- Gen.containerOfN[List, categories.Category](numCategories, Gen.oneOf(categories.Category.values))
     } yield
       ContextParams(datetime, datetimeOriginal, category.map(_.entryName), category.map(_.entryName).mkString(","))
+  }
+
+  implicit val context = Arbitrary[Context] {
+    for {
+      name       <- Gen.alphaStr
+      parameters <- arbitrary[ContextParams]
+      lifespan   <- Gen.posNum
+    } yield Context(name, parameters, lifespan)
+  }
+
+  implicit val metadata = Arbitrary[Metadata] {
+    for {
+      intentId                  <- Gen.alphaNumStr
+      webhookUsed               <- Gen.alphaStr
+      webhookForSlotFillingUsed <- Gen.alphaStr
+      intentName                <- Gen.alphaStr
+    } yield Metadata(intentId, webhookUsed, webhookForSlotFillingUsed, intentName)
+  }
+
+  implicit val message = Arbitrary[Message] {
+    for {
+      tpe    <- Gen.posNum
+      speech <- Gen.alphaStr
+    } yield Message(tpe, speech)
+  }
+
+  implicit val fulfillment = Arbitrary[Fulfillment] {
+    for {
+      speech   <- Gen.alphaStr
+      numMsg   <- Gen.choose(0, 5)
+      messages <- Gen.listOfN(numMsg, arbitrary[Message])
+    } yield Fulfillment(speech, messages)
+  }
+
+  implicit val result = Arbitrary[Result] {
+    for {
+      source           <- Gen.alphaStr
+      resolvedQuery    <- Gen.alphaUpperStr
+      speech           <- Gen.option(Gen.alphaStr)
+      action           <- Gen.alphaStr
+      actionIncomplete <- Gen.oneOf(true, false)
+      parameters       <- arbitrary[Parameters]
+      numContexts      <- Gen.choose(0, 10)
+      contexts         <- Gen.listOfN(numContexts, arbitrary[Context])
+      metadata         <- arbitrary[Metadata]
+      fulfillment      <- arbitrary[Fulfillment]
+      score            <- Gen.chooseNum(0.0, 100.0)
+    } yield
+      Result(source,
+             resolvedQuery,
+             speech,
+             action,
+             actionIncomplete,
+             parameters,
+             contexts,
+             metadata,
+             fulfillment,
+             score)
+  }
+
+  implicit val status = Arbitrary[Status] {
+    for {
+      code            <- Gen.posNum
+      errorType       <- Gen.alphaStr
+      webhookTimedOut <- Gen.option(Gen.oneOf(true, false))
+    } yield Status(code, errorType, webhookTimedOut)
+  }
+
+  implicit val df1Request = Arbitrary[DialogflowRequest] {
+    for {
+      id        <- Gen.alphaNumStr
+      lang      <- Gen.oneOf("en", "ru")
+      result    <- arbitrary[Result]
+      status    <- arbitrary[Status]
+      sessionId <- Gen.alphaNumStr
+    } yield DialogflowRequest(id, lang, result, status, sessionId)
   }
 }
 
