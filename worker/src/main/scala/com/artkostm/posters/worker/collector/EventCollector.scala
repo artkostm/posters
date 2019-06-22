85f4736ec10e4795ef8295230d10eacda5a910de
Artsiom <artkostm@gmail.com>
2019-06-22 22:19:28 +0300

    update event collector

--- worker/src/main/scala/com/artkostm/posters/worker/collector/EventCollector.scala
+++ worker/src/main/scala/com/artkostm/posters/worker/collector/EventCollector.scala
@@ -3,38 +3,51 @@ package com.artkostm.posters.worker.collector
 import java.time.LocalDate
 import java.time.temporal.ChronoUnit
 
+import cats.data.NonEmptyList
 import cats.effect.{Concurrent, Timer}
 import com.artkostm.posters.algebra.{EventStore, InfoStore, VisitorStore}
 import com.artkostm.posters.interfaces.event.EventInfo
-import com.artkostm.posters.scraper.Scraper
+import com.artkostm.posters.scraper.EventScraper
 import fs2._
+import org.log4s.getLogger
 
-class EventCollector[F[_]: Timer](scraper: Scraper[F],
+class EventCollector[F[_]: Timer](scrapers: NonEmptyList[EventScraper[F]],
                                   eventStore: EventStore[F],
                                   infoStore: InfoStore[F],
                                   visitorStore: VisitorStore[F])(implicit F: Concurrent[F]) {
+  private[this] val logger = getLogger
+
   // TODO: move all magic numbers to config
-  def collect() = {
-    val dayStream = Stream
-      .range(-2, 31)
-      .covary[F]
-      .map(LocalDate.now().plus(_, ChronoUnit.DAYS))
-      .mapAsyncUnordered(4)(scraper.event)
+  // TODO: add logging
+  def collect(days: Seq[LocalDate]) = {
 
-    val insertDays = dayStream.mapAsyncUnordered(4)(eventStore.save)
-    val saveEvents = dayStream
-      .flatMap(day => Stream.emits(day.categories.flatMap(_.events)))
-      .mapAsyncUnordered(4)(event => scraper.eventInfo(event.media.link))
-      .chunkN(10)
-      .mapAsync(4)(chunk =>
-        infoStore.save(chunk.map {
-          case Some(EventInfo(link, eventInfo)) => (link, eventInfo, eventInfo)
-        })
-      )
+    def collectWith(scraper: EventScraper[F]): Stream[F, Any] = {
+      println("Scrapping for: " + scraper)
+      val dayStream = Stream
+        .range(-2, 31)
+        .covary[F]
+        .map(LocalDate.now().plus(_, ChronoUnit.DAYS))
+        .mapAsyncUnordered(4)(scraper.event)
 
-    Stream.eval(eventStore.deleteOld(LocalDate.now())) >>
-      Stream.eval(infoStore.deleteOld()) >>
-      Stream.eval(visitorStore.deleteOlderThan(LocalDate.now())) >>
-      Stream(insertDays, saveEvents).parJoin(2)
+      val insertDays = dayStream.mapAsyncUnordered(4)(eventStore.save)
+      val saveEvents = dayStream
+        .flatMap(day => Stream.emits(day.categories.flatMap(_.events)))
+        .mapAsyncUnordered(4)(event => scraper.eventInfo(event.media.link))
+        .chunkN(10)
+        .mapAsync(4)(chunk =>
+          infoStore.save(chunk.map {
+            case Some(EventInfo(link, eventInfo)) => (link, eventInfo, eventInfo)
+          }))
+
+      Stream.eval(eventStore.deleteOld(LocalDate.now())) >>
+        Stream.eval(infoStore.deleteOld()) >>
+        Stream.eval(visitorStore.deleteOlderThan(LocalDate.now())) >>
+        Stream(insertDays, saveEvents).parJoin(2)
+    }
+
+    for {
+      scraper <- Stream.emits[F, EventScraper[F]](scrapers.toList)
+      _       <- collectWith(scraper)
+    } yield ()
   }
 }
